5bd41adc9bc134c68ee54fde5482e40d3e203b24
Victor Sena Molero <victorsenam@gmail.com>
2015-09-03 22:34:34 -0300

    Yertle the Turtle was the king of the pound

--- victorsenam/ojs/uva/10152/10152.cpp
+++ victorsenam/ojs/uva/10152/10152.cpp
@@ -1,5 +1,3 @@
-// WA
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,56 +5,28 @@ using namespace std;
 const int N = 207;
 const int S = 87;
 
-int n, a, b, ss, t;
-char temp[S];
-string ori[N];
-string des[N];
-int ord[N][2];
-map<string, int> mp;
+int n, t;
+char nm[N][S];
+char ob[N][S];
 
 int main () {
     scanf("%d", &t);
     while (t--) {
         scanf("%d", &n);
+        
+        for (int i = 0; i < n; i++)
+            scanf("\n%[0-9a-zA-Z .]s", nm+i);
+        for (int i = 0; i < n; i++)
+            scanf("\n%[0-9a-zA-Z .]s", ob+i);
 
-        for (int i = 0; i < n; i++) {
-            scanf("\n%[a-zA-Z .]s", temp);
-            ori[n-i-1] = temp;
-        }
-        for (int i = 0; i < n; i++) {
-            scanf("\n%[a-zA-Z .]s", temp);
-            des[n-i-1] = temp;
+        int j = n-1;
+        for (int i = n-1; i >= 0; i--) {
+            if (strcmp(nm[i], ob[j]) == 0)
+                j--;
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (ori[i] == des[j]) {
-                    ord[i][0] = j;
-                    break;
-                }
-            }
-            printf("%d ", ord[i][0]);
-        }
+        for (; j >= 0; j--)
+            printf("%s\n", ob[j]);
         printf("\n");
-
-        bool t = 0;
-        while (42) {
-            int mini = n;
-            for (int i = 1; i < n; i++)
-                if (ord[i][t] < ord[i-1][t])
-                    mini = min(mini, ord[i-1][t]);
-            
-            if (mini == n)
-                break;
-
-            printf("%s\n", des[mini].c_str());
-
-            int j = 0;
-            ord[0][!t] = mini;
-            for (int i = 1; i < n; i++) {
-                if (ord[i][t] != mini)
-                    ord[j++][!t] = ord[i][t];
-            }
-        }
     }
 }
