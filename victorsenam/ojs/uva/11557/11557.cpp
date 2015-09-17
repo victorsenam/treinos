eeb87335f91fc1ad4c78762d22cfb408516a4854
Victor Sena Molero <victorsenam@gmail.com>
2015-09-17 15:36:52 -0300

    11557

--- victorsenam/ojs/uva/11557/11557.cpp
+++ victorsenam/ojs/uva/11557/11557.cpp
@@ -4,62 +4,61 @@ using namespace std;
 
 const int N = 107;
 const int S = 257;
-const int M = 10000;
+const int M = 10007;
 const string ender = "***END***";
 
-int n;
-char names[N];
-string codes[N][M];
-int lin[N];
+int lins[N];
+string code[N][M];
+char name[N][S];
 char temp[S];
+int n;
+set<int> res;
+set<int>::iterator it;
+int maxi;
 
 int main () {
-	while (scanf("%d", &n) != EOF) {
-        int code = 0;
-		bool ok = 0;
-        for (code = 0; code <= n; code++) {
-            lin[code] = 0;
-            scanf(" %[ -~]", names[code]);
-            printf("%s\n", names[code]);
-            while (scanf(" %[ -~]", temp)) {
-                printf("%s\n", temp);
-                if (strcmp(temp, ender.c_str()) == 0)
-                    break;
-
-                int i, j;
-                j = 0;
-                for (i = 0; temp[i] != '\0'; i++)
-                    if (temp[i] != temp[i+1] || temp[i] != ' ')
-                        temp[j++] = temp[i];
-                temp[j++] = '\0';
-
-                codes[code][lin[code]++] = temp;
-            }
-		}
-
+    while (scanf("%d", &n) != EOF) {
         for (int i = 0; i <= n; i++) {
-            printf("* %s *\n", names[i]);
-            for (int j = 0; j < lin[i]; j++)
-                printf("%s\n", codes[i][j].c_str());
-            printf("===\n");
+            lins[i] = 0;
+            if (i < n)
+                scanf(" %[ -~]", name[i]);
+
+            while (scanf(" %[ -~]", temp) && strcmp(temp, ender.c_str())) {
+                int k = 0;
+                for (int j = 0; temp[j] != '\0'; j++)
+                    if (temp[j] != temp[j+1] || temp[j] != ' ')
+                        temp[k++] = temp[j];
+                temp[k++] = '\0';
+                if (k == 1)
+                    continue;
+                code[i][lins[i]++] = temp;
+            }
         }
 
-		int maxi = 0;
-		int vmax = -1;
-		for (int i = 0; i < n; i++) {
-			int att = 0;
-			for (int j = 0; j < min(lin[i], lin[n]); j++) {
-				if (codes[i][j] == codes[n][j])
-					att++;
-				if (att > maxi) {
-					vmax = i;
-					maxi = att;
-				}
-			}
-		}
-		if (maxi == 0)
-			printf("%d\n", 0);
-		else
-			printf("%s\n", names[vmax]);
-	}
+        maxi = 0;
+        res.clear();
+
+        for (int i = 0; i < n; i++) {
+            int att = 0;
+            for (int j = 0; j < min(lins[i], lins[n]); j++) {
+                if (strcmp(code[i][j].c_str(), code[n][j].c_str()) == 0)
+                    att++;
+
+                if (!att)
+                    continue;
+
+                if (att > maxi) {
+                    maxi = att;
+                    res.clear();
+                }
+                if (maxi == att)
+                    res.insert(i);
+            }
+        }
+
+        printf("%d", maxi);
+        for (it = res.begin(); it != res.end(); it++)
+            printf(" %s", name[*it]);
+        printf("\n");
+    }
 }
