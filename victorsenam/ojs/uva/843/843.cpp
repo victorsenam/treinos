f3bf3b37ac7c43a7e281394034ec3e26e7752c8e
Victor Sena <victorsenam@gmail.com>
2015-09-18 15:02:28 -0300

    ainda nao... blergh

--- victorsenam/ojs/uva/843/843.cpp
+++ victorsenam/ojs/uva/843/843.cpp
@@ -10,10 +10,42 @@ int n, m;
 string dic[N];
 string wrd[T];
 char inp[T];
+char keep[T];
 char * tmp;
+char mp[256];
+int used[256];
 
 bool siz_cmp (const string & a, const string & b) {
     return b.length() < a.length();
+}
+
+bool solve (int w) {
+    printf("%s\n", wrd[w].c_str());
+    if (w == m)
+        return 1;
+    for (int lo = lower_bound(dic, dic+n, wrd[w], siz_cmp) - dic; dic[lo].length() == wrd[w].length(); ++lo) {
+        int i = 0;
+        for (i = 0; i < wrd[w].length(); i++) {
+            if (mp[wrd[w][i]] == '*') {
+                if (used[dic[lo][i]])
+                    break;
+                mp[wrd[w][i]] = dic[lo][i];   
+                used[dic[lo][i]] = w + 1;
+            } else if (mp[wrd[w][i]] != dic[lo][i])
+                break;
+        }
+
+        if (i == n && solve(w+1))
+            return 1;
+
+        for (int j = 0; j < i; j++) {
+            if (used[mp[wrd[w][j]]] == w + 1) {
+                used[mp[wrd[w][j]]] = 0;
+                mp[wrd[w][j]] = '*';
+            }
+        }
+    }
+    return 0;
 }
 
 int main () {
@@ -26,6 +58,9 @@ int main () {
 
     while (scanf(" %[ a-z]", inp) != EOF) {
         m = 0;
+        for (int i = 0; i && inp[i-1] != '\0'; i++)
+            keep[i] = inp[i];
+
         tmp = strtok(inp, " \0");
         while (tmp != NULL) {
             wrd[m++] = tmp;
@@ -34,7 +69,16 @@ int main () {
 
         sort(wrd, wrd+m, siz_cmp);
 
-        for (int i = 0; i < m; i++)
-            printf("%s\n", wrd[i].c_str());
+        for (char a = 'a'; a <= 'z'; a++) {
+            used[a] = 0;
+            mp[a] = '*';
+        }
+        mp[' '] = ' ';
+
+        solve(0);
+
+        for (int i = 0; keep[i] != '\0'; i++)
+            printf("%c", mp[keep[i]]);
+        printf("\n");
     }
 }
