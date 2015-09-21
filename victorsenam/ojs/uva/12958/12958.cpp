ef104b474b8cde800c096c63cf67577dc19fc75f
Victor Sena <victorsenam@gmail.com>
2015-09-21 20:01:12 -0300

    I comecinho

---
+++ victorsenam/ojs/uva/12958/12958.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef bitset<100> num;
+
+const int N = 31;
+const int K = 12;
+const int mi[4] = {0, 0, 1, -1};
+const int mj[4] = {1, -1, 0, 0};
+
+#define ff first
+#define ss second
+
+set<vii> s[K];
+set<vii>::iterator it;
+vii aux, att;
+int t;
+int n, m, h;
+int k;
+
+int main () {
+    
+    for (int i = 1; i <= 10; i++) {
+        if (i == 1) {
+            aux.push_back(make_pair(0, 0));
+            s[1].insert(aux);
+        } else {
+            for (it = s[i-1].begin(); it != s[i-1].end(); it++) {
+                aux = *it;
+
+                for (int j = 0; j < aux.size(); j++) {
+                    for (int k = 0; k < 4; k++) {
+                        att = aux;
+                        att.push_back(make_pair(aux[j].ff + mi[j], aux[j].ss + mj[j]));
+
+                        sort(att.begin(), att.end());
+                        bool un = 1;
+                        for (int l = 0; un && l < att.size() - 1; l++)
+                            if (att[l].ff == att[l+1].ff && att[l].ss == att[l+1].ss)
+                                un = 0;
+                        if (!un)
+                            continue;
+                        for (int l = att.size()-1; l >= 0; l++) {
+                            att[l].ff -= att[0].ff;
+                            att[l].ss -= att[0].ss;
+                        }
+                        s[i].insert(att);
+                    }
+                }
+            }
+        }
+    }
+}
