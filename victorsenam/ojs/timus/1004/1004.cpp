ae7935f730ded913bbca79b59ad7ce676090713a
Victor Sena Molero <victorsenam@gmail.com>
2015-10-01 15:45:09 -0300

    1004 em progresso

--- victorsenam/ojs/timus/1004/1004.cpp
+++ victorsenam/ojs/timus/1004/1004.cpp
@@ -1,3 +1,5 @@
+// TODO: Tirar a Pq do Dijkstra
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -5,86 +7,84 @@ using namespace std;
 const int N = 101;
 const int M = N*N;
 
-int dist[2][N][N];
-int from[2][N][N];
-int a[2*M], b[2*M], w[2*M];
+struct vert {
+    int v, w, p;
+
+    bool operator < (const vert & a) const
+    { return w < a.w; }
+};
+
+int nx[M], hd[M], hg[M], to[M];
+int es;
 int n, m;
+int a, b, l;
+int turn;
+vert best[N];
+int rw;
+int locw, locm;
+deque<int> res;
+
+void montaRes(vert aux) {
+    res.clear();
+    rw = aux.w + best[aux.v].w;
+}
+
+void djs(int ini) {
+    priority_queue<vert> pq;
+    turn++;
+
+    vert att;
+    att.v = att.p = ini;
+    att.w = 0;
+    pq.push(att);
+
+    for (int i = n; i >= 1; i--) {
+        att = best[ord[0]];
+        for (int j = 1; i < n; j++)
+            att = min(att, best[ord[j]]);
+
+        if (att.w >= rw)
+            break;
+
+        for (int ed = hd[att.v]; ed != -1; ed = nx[ed]) {
+            vert aux = att;
+            aux.w += hg[ed];
+            aux.v = to[ed];
+            aux.p = att.v;
+
+            if (aux.v == att.p)
+                continue;
+            
+            pq.push(aux);
+        }
+    }
+}
 
 int main () {
     while (scanf("%d %d", &n, &m) != EOF && n != -1) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dist[0][i][j] = dist[1][i][j] = i==j?0:INT_MAX;
-                from[0][i][j] = from[1][i][j] = i;
-            }
-        }
+        es = 0;
+        for (int i = 0; i < n; i++)
+            hd[i] = -1;
 
         for (int i = 0; i < m; i++) {
-            scanf("%d %d %d", a+i, b+i, w+i);
-            a[i]--; b[i]--;
-            a[i+m] = b[i];
-            b[i+m] = a[i];
-            w[i+m] = w[i];
-            dist[0][a[i]][b[i]] = min(dist[0][a[i]][b[i]], w[i]);
-            dist[0][b[i]][a[i]] = dist[0][a[i]][b[i]];
-            dist[1][a[i]][b[i]] = dist[0][a[i]][b[i]];
-            dist[1][b[i]][a[i]] = dist[0][b[i]][a[i]];
+            scanf("%d %d %d", &a, &b, &l);
+            a--; b--;
+            nx[es] = hd[a]; hg[es] = l; hd[a] = es; to[es++] = b;
+            nx[es] = hd[b]; hg[es] = l; hd[b] = es; to[es++] = a;
         }
 
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (max(dist[0][i][k], dist[0][k][j]) < INT_MAX && dist[0][i][j] > dist[0][i][k] + dist[0][k][j]) {
-                        dist[0][i][j] = dist[0][i][k] + dist[0][k][j];
-                        from[0][i][j] = from[0][k][j];
-                    }
-                }
-            }
-        }
+        rw = INT_MAX;
+        for (int i = 0; i < n; i++)
+            djs(i);
 
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (from[0][i][j] != from[1][k][j] && max(dist[1][i][k], dist[1][k][j]) < INT_MAX && dist[1][i][j] > dist[1][i][k] + dist[1][k][j]) {
-                        dist[1][i][j] = dist[1][i][k] + dist[1][k][j];
-                        from[1][i][j] = from[1][k][j];
-                    }
-                }
-            }
+        if (rw == INT_MAX) {
+            printf("No solution.\n");
+            continue;
         }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[0][i][j] == INT_MAX)
-                    printf("-1 ");
-                else 
-                    printf("%d ", dist[0][i][j]);
-            }
-            printf("\n");
+        while (!res.empty()) {
+            printf("%d ", res.front()+1);
+            res.pop_front();
         }
-        printf("==\n");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dist[1][i][j] == INT_MAX)
-                    printf("-1 ");
-                else 
-                    printf("%d ", dist[1][i][j]);
-            }
-            printf("\n");
-        }
-        
-        int res = INT_MAX;
-        for (int k = 0; k < 2*m; k++) {
-            for (int i = 0; i < n; i++) {
-                if (i == a[k] || i == b[k])
-                    continue;
-                if (dist[0][i][a[k]] == INT_MAX || dist[1][b[k]][i] == INT_MAX)
-                    continue;
-                res = min(res, dist[0][i][a[k]]+dist[1][b[k]][i]+w[k]);
-                printf("%d -> %d <- %d = %d\n", a[k]+1, i+1, b[k]+1, dist[0][i][a[k]]+dist[1][b[k]][i]+w[k]);
-            }
-        }
-        printf("%d\n", res);
-        printf("==\n");
+        printf("\n");
     }
 }
