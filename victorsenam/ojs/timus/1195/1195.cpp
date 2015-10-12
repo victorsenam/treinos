22db3d81bd3183c94ee17cbc2f05d7f6483bb282
Victor Sena Molero <victorsenam@gmail.com>
2015-10-12 20:11:09 -0300

    1195

--- victorsenam/ojs/timus/1195/1195.cpp
+++ victorsenam/ojs/timus/1195/1195.cpp
@@ -1,89 +1,92 @@
-// WA
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
 char mat[3][3];
-int col[3], row[3], di[2];
 
-int bt (int l) {
-    if (l == 0)
+inline int val (char c) {
+    if (c == 'X')
+        return 1;
+    else if (c == 'O')
+        return -1;
+    else
         return 0;
-
-    if (l%2) { // X
-        int res = -1;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (mat[i][j] != '#')
-                    continue;
-                if (row[i] + 1 == 3 || col[j] + 1 == 3)
-                    return 1;
-                if (i == j && di[0] + 1 == 3)
-                    return 1;
-                if (2-i == j && di[1] + 1 == 3)
-                    return 1;
-                row[i]++;
-                col[j]++;
-                di[0] += (i==j);
-                di[1] += (2-i==j);
-                mat[i][j] = 'X';
-                res = max(res, bt(l-1));
-                if (res == 1)
-                    return 1;
-                row[i]--;
-                col[j]--;
-                di[0] -= (i==j);
-                di[1] -= (2-i==j);
-                mat[i][j] = '#';
-            }
-        }
-        return res;
-    } else { // O
-        int res = 1;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (mat[i][j] != '#')
-                    continue;
-                if (row[i] - 1 == -3 || col[j] - 1 == -3)
-                    return -1;
-                if (i == j && di[0] - 1 == -3)
-                    return -1;
-                if (2-i == j && di[1] - 1 == -3)
-                    return -1;
-                row[i]--;
-                col[j]--;
-                di[0] -= (i==j);
-                di[1] -= (2-i==j);
-                mat[i][j] = 'O';
-                res = min(res, bt(l-1));
-                if (res == -1)
-                    return -1;
-                row[i]++;
-                col[j]++;
-                di[0] += (i==j);
-                di[1] += (2-i==j);
-                mat[i][j] = '#';
-            }
-        }
-        return res;
-    }
 }
 
-int main ( ) {
-    for (int i = 0; i < 3; i++) {
-        scanf("%s", mat[i]);
+inline int calc (int v) {
+    if (v == -3)
+        return -1;
+    else if (v == 3)
+        return 1;
+    else
+        return 0;
+}
+
+int minimax (int l) {
+    if (l == -1)
+        return 0;
+    int part;
+    int res = 0;
+
+    int dia = 0;
+    int dib = 0;
+    for (int i = 0; !res && i < 3; i++) {
+        int row = 0;
+        int col = 0;
+        dia += val(mat[i][i]);
+        dib += val(mat[2-i][i]);
         for (int j = 0; j < 3; j++) {
-            row[i] += mat[i][j]=='#'?0:(mat[i][j]=='X'?1:-1);
-            col[j] += mat[i][j]=='#'?0:(mat[i][j]=='X'?1:-1);
-            if (i == j)
-                di[0] += mat[i][j]=='#'?0:(mat[i][j]=='X'?1:-1);
-            else if (2-i == j)
-                di[1] += mat[i][j]=='#'?0:(mat[i][j]=='X'?1:-1);
+            row += val(mat[i][j]);
+            col += val(mat[j][i]);
+        }
+        if (calc(row))
+            res = calc(row);
+        if (calc(col))
+            res = calc(col);
+    }
+
+    if (res);
+    else if (calc(dia))
+        res = calc(dia);
+    else if (calc(dib))
+        res = calc(dib);
+    else if (l == 0)
+        res = 0;
+    else {
+        res = l%2?-1:1;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (mat[i][j] != '#')
+                    continue;
+                if (l%2) {
+                    mat[i][j] = 'X';
+                    res = max(minimax(l-1), res);
+                } else {
+                    mat[i][j] = 'O';
+                    res = min(minimax(l-1), res);
+                }
+                mat[i][j] = '#';
+            }
         }
     }
 
-    int res = bt(3);
+/*
+    printf("%d -> %d\n", l,  res);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++)
+            printf("%c", mat[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+*/
+    return res;
+}
+
+int main () {
+    for (int i = 0; i < 3; i++)
+        scanf(" %s", mat[i]);
+
+    int res = minimax(3);
     if (res == 1)
         printf("Crosses win\n");
     else if (res == -1)
