ebfd9de59fbf2a950bcfcacb6d59fe49ad647ee6
Victor Sena Molero <victorsenam@gmail.com>
2015-11-08 23:59:14 -0200

    convex trick do sucesso

--- victorsenam/ojs/icpc/6131/6131.cpp
+++ victorsenam/ojs/icpc/6131/6131.cpp
@@ -1,65 +1,78 @@
-// INCOMPLETO - REFAZER TALVEZ
-
 #include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1007;
 
 typedef long long int num;
 
-const int N = 1003;
+struct crs {
+    double s;
+    num a, b;
 
-num dp[2][N];
+    num calc (num x)
+    { return a*x + b; }
+
+    double cross (crs & other)
+    { return double(other.b-b)/double(a-other.a); }
+};
+
 int n, k;
 num x[N], w[N];
-num ini[N];
-num acc[N];
-double crs[N];
-num a[N], b[N];
+num dp[2][N], acc[N], sum[N];
+crs qu[N];
 int qi, qf;
 
-double clccrs(int i, int j) {
-    return  double(b[j]-b[i])/double(a[j]-a[i]);
-}
-
 int main () {
-    while (scanf("%d %d", &n, &k) != EOF) {
+    while (scanf("%d %d", &n, &k) != EOF && n && k) {
         for (int i = 0; i < n; i++)
             scanf("%lld %lld", x+i, w+i);
 
-
-        dp[0][0] = 0;
+        sum[0] = 0;
         for (int i = 0; i < n; i++)
-            acc[i] = dp[0][i] = dp[0][i - !!i] + x[i];
+            sum[i] = sum[i-!!i] + w[i];
+
+        acc[0] = 0;
+        for (int i = 0; i < n; i++)
+            acc[i] = dp[1][i] = acc[i-!!i] + sum[i-!!i]*(x[i]-x[i-!!i]);
 
         for (int q = 2; q <= k; q++) {
-            bool t = !(q&1);
-                
+            bool t = q&1;
             qi = qf = 0;
 
-            for (int i = q-1; i < n; i++) { 
-                a[qf] = -i;
-                b[qf] = dp[!t][i-1] + acc[i-1];
-                crs[qf] = 0;
-                
-                // Colocar a nova reta na posição certa
+            dp[t][q-1] = dp[!t][q-2];
+
+            for (int i = q-1; i < n; i++) {
+                int j = i-1;
+                crs aux;
+                aux.a = -sum[j];
+                aux.b = dp[!t][j]-acc[j]+sum[j]*x[j];
+                aux.s = -1000.0;
+
                 while (qi < qf) {
-                    crs[qf] = clccrs(qf, qf-1);
-                    if (crs[qf] <= crs[qf-1]) {
-                        a[qf-1] = a[qf];
-                        b[qf-1] = b[qf];
-                        crs[qf-1] = crs[qf];
-                        qf--;
-                    } else
+                    qf--;
+                    aux.s = aux.cross(qu[qf]);
+                    if (aux.s > qu[qf].s) {
+                        qf++;
                         break;
+                    }
                 }
-                qf++;
+                qu[qf++] = aux;
 
-                // Ignorar as retas que terminam antes de x[i]
-                while (++qi < qf && crs[qi] <= x[i]);
-                qi--;
-
-                dp[t][i] = a[qi]*x[i] + b[qi] + (i+1)*x[i] - acc[i-1];
+                dp[t][i] = qu[qi].calc(x[i]) + acc[i];               
+                
+                while (qi+1 < qf) {
+                    num val = qu[qi+1].calc(x[i]) + acc[i];
+                    if (val <= dp[t][i]) {
+                        dp[t][i] = val;
+                        qi++;
+                    } else {
+                        break;
+                    }
+                }
             }
         }
 
-        printf("%lld\n", dp[!(k&1)][n-1]);
+        printf("%lld\n", dp[k&1][n-1]);
     }
 }
