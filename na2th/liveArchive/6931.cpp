4bf4e71506c4c46d60d8553070384684f9740b38
nathanPro <nathan@vieiraproenca.com>
2015-11-16 22:02:39 +0000

    Cant stop playing it

--- na2th/liveArchive/6931.cpp
+++ na2th/liveArchive/6931.cpp
@@ -1,50 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef int num;
-const int MS = (1<<13)+3;
-const int MN = 1002;
-num sol[MN][MS], seen[MN][MS], tc, in[MN], acc[MN], n;
+const int MS = (1<<13)+300;
+const int MN = 1.1e3;
 
-num solve(int i, int s)
+num sol[MN][MS];
+num in[MN], acc[MN], tgt, nxt;
+char out[MN][MS];
+int tc, n;
+
+inline bool power(num x)
+{ return !(x - (x&-x)); }
+
+void build(int i, int l)
 {
-    if( i == n )
-        return (s == 0 && !(acc[n]&(acc[n]-1)) ) ||
-               (2*s == acc[n] && !(s&(s-1)))       ||
-               (s == acc[n] && !(s&(s-1))) ;
-
-    if( seen[i][s] != tc )
-    {
-        int r = acc[i]-s;
-        seen[i][s] = tc;
-        sol[i][s] = 0;
-
-        if( in[i] <= (r&(-r)) || r == 0 )
-            sol[i][s] |= solve(i+1, s);
-
-        if( in[i] <= (s&(-s)) || s == 0 )
-            sol[i][s] |= solve(i+1, s+in[i]);
-
-    }
-    return sol[i][s];
+    assert(sol[i][l] != -1);
+    if( i == n+1 ) return;
+    putchar(out[i][l]);
+    build(i+1, sol[i][l]);
 }
 
 int main()
 {
-    scanf("%d", &tc);
+    scanf(" %d", &tc);
     while(tc--)
     {
-        scanf("%d", &n);
-        acc[0] = as = 0;
-        for(int i=0;i<n;i++)
+        scanf(" %d", &n);
+        for(int i=1;i<n+1;i++)
         {
-            scanf("%d", in+i);
-            acc[i+1] = acc[i] + in[i];
+            scanf(" %d", in+i);
+            acc[i] = acc[i-1] + in[i];
         }
-        if(solve(0,0))
-            while( as > 0 )
-                printf("%c", ans[--as]);
-        else
-            printf("no");
+
+        for(int l=0;l<acc[n]+1;l++)
+        {
+            int r = acc[n] - l;
+            sol[n+1][l] = -1;
+            if( r == 0 && power(l) ) sol[n+1][l] = 1;
+            if( l == 0 && power(r) ) sol[n+1][l] = 1;
+            if( r == l && power(l) ) sol[n+1][l] = 1;
+        }
+        for(int i=n;i>0;--i)
+            for(int l=0;l<acc[i-1]+1;l++)
+            {
+                int r = acc[i-1] - l;
+                sol[i][l] = -1;
+                out[i][l] = 0;
+                if( l == 0 || in[i] <= (l&-l) )
+                {
+                    tgt = l + in[i];
+                    nxt = ( power(tgt) && (tgt<<1) > r ) ? 0 : tgt;
+                    if( sol[i+1][nxt] != -1 )
+                    {
+                        sol[i][l] = nxt;
+                        out[i][l] = 'l';
+                    }
+                }
+
+                if( ( r == 0 || in[i] <= (r&-r) ) && ( sol[i+1][l] != -1 ) )
+                {
+                    sol[i][l] = l;
+                    out[i][l] = 'r';
+                }
+            }
+
+        if( sol[1][0] != -1 ) build(1,0);
+        else printf("no");
         printf("\n");
     }
 }
