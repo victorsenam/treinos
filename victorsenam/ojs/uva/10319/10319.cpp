b25ec56804077065ce66e40a6e22aeec6add9524
Victor Sena <victorsenam@gmail.com>
2015-11-16 19:14:41 -0200

    Um fluxo e o manhattan, chupa todo mundo

--- victorsenam/ojs/uva/10319/10319.cpp
+++ victorsenam/ojs/uva/10319/10319.cpp
@@ -3,45 +3,108 @@
 
 using namespace std;
 
-const int N = 300;
-const int M = 1000;
+const int N = 100000;
+const int M = 100000;
 
 int hd[N], to[M], nx[M], es;
-int as, bs, n, t;
+int na, nb, m;
+int a[2], b[2];
+int open[N], clse[N], turn;
+int pre[N], low[N], cnt;
+int cmp[N], cs;
+int st[N], ss;
+
+inline void connect (int a, int b)
+{ nx[es] = hd[a]; hd[a] = es; to[es] = b; es++; }
+
+void conds (int a1, int a2, int b1, int b2) {
+    connect(a1^1, a2);
+    connect(a1^1, b1);
+}
+
+int dfs (int u) {
+    if (clse[u] == turn)
+        return INT_MAX;
+    if (open[u] == turn)
+        return pre[u];
+
+    open[u] = turn;
+    pre[u] = low[u] = cnt++;
+    st[ss++] = u;
+    
+    for (int ed = hd[u]; ed != -1; ed = nx[ed])
+        low[u] = min(low[u], dfs(to[ed]));
+
+    clse[u] = turn;
+
+    if (pre[u] == low[u]) {
+        while (st[ss] != u) {
+            cmp[st[--ss]] = cs;
+        }
+        cs++;
+    }
+
+    return low[u];
+}
 
 int main () {
+    int t;
     scanf("%d", &t);
     while (t--) {
-        scanf("%d %d %d ", &as, &bs, &n);
+        memset(hd, -1, sizeof hd);
 
-        for (int i = 0; i < n; i++) {
-            int a[2], b[2];
-            scanf("%d %d %d %d", a, b, a+1, b+1);
-
+        scanf("%d %d %d", &na, &nb, &m);
+        
+        for (int i = 0; i < m; i++) {
             for (int k = 0; k < 2; k++) {
-                a[k] <<= 1;
-                b[k] += as;
-                b[k] <<= 1;
+                scanf("%d %d", a+k, b+k);
+                a[k]--; b[k] += na - 1;
+                a[k] <<= 1; b[k] <<= 1;
             }
+            
+            if (a[0] < a[1])
+            { b[0] ^= 1; b[1] ^= 1; }
+
+            if (b[0] < b[1])
+            { a[0] ^= 1; a[1] ^= 1; }
+
 
             if (a[0] == a[1]) {
-                if (b[0] < b[1])
-                    a[0] |= 1;
                 connect(a[0]^1, a[0]);
             } else if (b[0] == b[1]) {
-                if (a[0] < a[1])
-                    a[0] |= 1;
                 connect(b[0]^1, b[0]);
             } else {
-                for (int k = 0; k < 2; k++) {
-                    if (a[0] < a[1])
-                        b[k] |= 1;
-                    if (b[0] < b[1])
-                        a[k] |= 1;
-
-                    connect(
-                }
+                conds(a[0], a[1], b[0], b[1]);
+                conds(a[1], a[0], b[1], b[0]);
+                conds(b[0], b[1], a[0], a[1]);
+                conds(b[1], b[0], a[1], a[0]);
             }
         }
+
+/*
+        for (int i = 0; i < (na+nb)<<1; i++) {
+            printf("%d:", i);
+            for (int ed = hd[i]; ed != -1; ed = nx[ed])
+                printf(" %d", to[ed]);
+            printf("\n");
+        }
+*/
+
+        cnt = cs = ss = 0;
+        cs++;
+        turn++;
+        for (int i = 0; i < (na+nb)<<1; i++)
+            dfs(i);
+        
+        bool ok = 1;
+        for (int i = 0; (ok|1) && i < (na+nb)<<1; i += 2) {
+            if (cmp[i] == cmp[i+1])
+                ok = 0;
+        }
+
+        if (ok)
+            printf("Yes\n");
+        else
+            printf("No\n");
     }
 }
--- victorsenam/ojs/uva/13005/13005.cpp
+++ victorsenam/ojs/uva/13005/13005.cpp
@@ -1,8 +1,144 @@
-// CLARAMENTE INCOMPLETO
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main () {
+const int N = 400;
+const int M = 1000000;
 
+int he[N], hd[N], to[M], nx[M], fl[M], cp[M], es;
+int src, snk;
+int n, qs, m;
+bool comp;
+int q[N], qi, qf;
+int dist[N], visi[N], turn;
+
+void connect(int a, int b, int c) {
+    nx[es] = hd[a]; hd[a] = es; to[es] = b; fl[es] = 0; cp[es] = c; es++;
+    nx[es] = hd[b]; hd[b] = es; to[es] = a; fl[es] = 0; cp[es] = 0; es++;
+}
+
+int bfs() {
+    qi = qf = 0;
+    q[qf++] = snk;
+    turn++;
+
+    for (int i = 0; i <= snk; i++)
+        dist[i] = INT_MAX;
+
+    dist[snk] = 0;
+
+    while (qi < qf) {
+        int u = q[qi++];
+        if (visi[u] == turn)
+            continue;
+        visi[u] = turn;
+
+        for (int ed = hd[u]; ed != -1; ed = nx[ed]) {
+            if (visi[to[ed]] == turn || dist[to[ed]] <= dist[u]+1 || fl[ed^1] >= cp[ed^1])
+                continue;
+            dist[to[ed]] = min(dist[to[ed]], dist[u]+1);
+            q[qf++] = to[ed];
+        }
+    }
+
+    return dist[src];
+}
+
+int dfs(int u, int r) {
+    if (r == 0)
+        return 0;
+
+    if (u == snk)
+        return r;
+
+    for (int & ed = he[u]; ed != -1; ed = nx[ed]) {
+        if (dist[u] != dist[to[ed]] + 1 || cp[ed] <= fl[ed])
+            continue;
+
+        int loc = dfs(to[ed], min(r, cp[ed]-fl[ed]));
+        if (loc) {
+            fl[ed] += loc;
+            fl[ed^1] -= loc;
+         //   printf("%d ", u);
+            return loc;
+        }
+    }
+    return 0;
+}
+
+int dinic () {
+    int res = 0;
+    while (bfs() < INT_MAX) {
+        for (int i = 0; i <= snk; i++)
+            he[i] = hd[i];
+        // printf("============ \n");
+        while (int loc = dfs(src, INT_MAX)) {
+           // printf("%d \n", loc);
+            res += loc;
+        }
+    }
+    return res;
+}
+
+int main () {
+    while (scanf("%d %d", &n, &qs) != EOF) {
+        memset(hd, -1, sizeof hd);
+
+        src = n+n;
+        snk = src+1;
+        comp = 0;
+        
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &m);
+            if (n == m)
+                comp = 1;
+
+            connect(src, i, 1);
+
+            for (int j = 0; j < m; j++) {
+                int a;
+                scanf("%d", &a);
+                a--;
+
+                connect(i, n+a, 1);
+            }
+        }
+
+
+        for (int i = 0; i < n; i++)
+            connect(n+i, snk, 0);
+
+        for (int i = 0; i < qs; i++) {
+            scanf("%d", &m);
+            if (m == 0) {
+                if (comp)
+                    printf("N\n");
+                else
+                    printf("Y\n");
+                continue;
+            }
+
+            if (i) {
+                memset(fl, 0, sizeof fl);
+
+                for (int j = 0; j < n; j++) 
+                    cp[hd[n+j]] = 0;
+            }
+
+            for (int j = 0; j < m; j++) {
+                int a;
+                scanf("%d", &a);
+                a--;
+                
+                cp[hd[n+a]] = 1;
+            }
+
+            int val = dinic();
+ //           printf("%d\n", val);
+            if (val == m)
+                printf("Y\n");
+            else
+                printf("N\n");
+        }
+    }    
 }
