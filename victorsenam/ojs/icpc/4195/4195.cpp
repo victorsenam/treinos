f99196c50c943fbd6ae5c0a21c6f039c4ed47152
Victor Sena Molero <victorsenam@gmail.com>
2015-11-18 09:34:20 -0200

    Lawrence of Arabiaaaaaaa

--- victorsenam/ojs/icpc/4195/4195.cpp
+++ victorsenam/ojs/icpc/4195/4195.cpp
@@ -7,35 +7,51 @@ typedef long long int num;
 
 num dp[2][N];
 int n, k;
-int x[N];
+num x[N];
+num cost[N][N];
 
-int main ( ) {
+void solve (int lo, int hi, int l, int r, bool ty) {
+    if (lo == hi)
+        return;
+
+    int mid = (lo+hi)/2;
+    
+    int ind = mid;
+    num res = dp[!ty][mid];
+
+    for (int i = l; i <= min(r, mid); i++) {
+        num loc = dp[!ty][i-!!i] + cost[i][mid];
+        if (loc < res) {
+            res = loc;
+            ind = i;
+        }
+    }
+    dp[ty][mid] = res;
+
+    solve(lo, mid, l, ind, ty);
+    solve(mid+1, hi, ind, r, ty);
+}
+
+int main () {
     while (scanf("%d %d", &n, &k) != EOF && n) {
         for (int i = 0; i < n; i++)
-            scanf("%d", x+i);
+            scanf("%lld", x+i);
 
-        dp[0][0] = 0;
-        num acc = 0, cst;
         for (int i = 0; i < n; i++) {
-            dp[0][i] = dp[0][i-!!i] + acc*x[i];
-            acc += x[i];
-        }
-
-        for (int q = 1; q <= k; q++) {
-            bool ty = (q&1);
-            for (int i = 0; i < n; i++) {
-                acc = x[i]; 
-                cst = 0;
-                dp[ty][i] = dp[!ty][i-1];
-
-                for (int j = i-1; j >= 0; j--) {
-                    cst += acc*x[j];
-                    dp[ty][i] = min(dp[ty][i], dp[!ty][j-!!j] + cst);
-                    acc += x[j];
-                }
+            num acc = x[i];
+            cost[i][i] = 0;
+            for (int j = i+1; j < n; j++) {
+                cost[i][j] = cost[i][j-1] + x[j]*acc;
+                acc += x[j];
             }
         }
 
+        for (int i = 0; i < n; i++)
+            dp[0][i] = cost[0][i];
+
+        for (int q = 1; q <= k; q++) {
+            solve(0, n, 0, n, q&1);
+        }
         printf("%lld\n", dp[k&1][n-1]);
     }
 }
