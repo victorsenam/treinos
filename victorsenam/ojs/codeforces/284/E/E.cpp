f22b345215e5577c4ea453af72b5499b01cbedf4
Victor Sena Molero <victorsenam@gmail.com>
2015-12-05 18:20:31 -0200

    Mas aquele 1% é vagabundo

--- victorsenam/ojs/codeforces/284/E/E.cpp
+++ victorsenam/ojs/codeforces/284/E/E.cpp
@@ -1,58 +1,71 @@
-// SONO DEMAIS :(
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
+typedef long long int num;
 const int N = 307;
+const int T = 1e5+7;
+const num MOD = 1e9+7;
 
-int nx[N], vis[N], u[N];
-int att;
-int v[N], val[N];
-int p[N];
-int o;
+num mod (num a)
+{ return (a%MOD + MOD)%MOD; }
+
 int n, q, t, a, b;
+int nx[N], v[N], sum[N];
+int visi[N];
+num pd[2][T];
 
-int dfs (int i) {
-    if (i == 0)
+int dfs (int u) {
+    if (u == 0)
         return 0;
-    if (vis[i]) {
+    if (visi[u] == 1) {
         printf("0\n");
         exit(0);
-    } 
+    }
+    if (visi[u] == 2)
+        return sum[u];
 
-    p[o++] = i;
-    vis[i] = 1;
-    val[i] = v[i] + dfs(nx[i]);
+    sum[u] = v[u];
+    visi[u] = 1;
 
-    return val[i];
+    sum[u] += dfs(nx[u]);
+
+    visi[u] = 2;
+    return sum[u];
 }
 
 int main () {
     scanf("%d %d %d", &n, &q, &t);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
         scanf("%d", v+i);
 
     for (int i = 0; i < q; i++) {
         scanf("%d %d", &b, &a);
         nx[a] = b;
-        u[b] = 1;
     }
 
-    for (int i = 0; i < q; i++)
-        t -= val[i] - v[i];
-
-    printf("%d\n", t);
-    return 0;
-
-    bool ok = 0;
-    for (int i = 0; i < q; i++) {
-        if (u[i])
-            continue;
-        ok = 1;
+    for (int i = 1; i <= n; i++)
         dfs(i);
+
+    for (int i = 1; i <= n; i++) {
+        t -= sum[i] - v[i];
+        if (t < 0) {
+            printf("0\n");
+            return 0;
+        }
     }
 
-   
+    pd[0][0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        pd[i&1][0] = 1;
+        for (int k = 1; k <= t; k++) {
+            pd[i&1][k] = pd[!(i&1)][k];
+            if (k >= sum[i])
+                pd[i&1][k] = mod(pd[i&1][k] + pd[i&1][k-sum[i]]);
+        }
+    }
+
+    printf("%d\n", pd[n&1][t]);
 }
