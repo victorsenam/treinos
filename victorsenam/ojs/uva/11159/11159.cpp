5e7682937ac79a7085553a350e027f1f6551ad82
Victor Sena Molero <victorsenam@gmail.com>
2015-12-11 12:40:21 -0200

    Factors and Primes

--- victorsenam/ojs/uva/11159/11159.cpp
+++ victorsenam/ojs/uva/11159/11159.cpp
@@ -2,59 +2,25 @@
 
 using namespace std;
 
-const int N = 103;
-const int M = 600;
+const int N = 107;
+const int M = 40007;
 
-int hd[N], ht[N], to[M], nx[M], us[M], es;
-int a[N], b[N];
 int n, m;
-int tt;
-int src, snk;
-int qu[N], qi, qf;
-int dist[N];
+int mat[N];
+int hd[N], nx[M], to[M], es;
 int turn;
-int visi[N];
+int a[N], b[N];
+int vis[N];
 
-inline void connect (int u, int v) {
-    nx[es] = hd[u]; hd[u] = es; to[es] = v; us[es] = 0; es++;
-    nx[es] = hd[v]; hd[v] = es; to[es] = u; us[es] = 1; es++;
-}
-
-int bfs() {
-    qi = qf = 0;
-    turn++;
-    qu[qf++] = snk;
-    dist[snk] = 0;
-    visi[snk] = turn;
-
-    while (qi < qf) {
-        int u = qu[qi++];
-        for (int ed = hd[u]; ed != -1; ed = nx[ed]) {
-            if (us[ed^1])
-                continue;
-            if (visi[to[ed]] == turn && dist[to[ed]] <= dist[u]+1)
-                continue;
-            visi[to[ed]] = turn;
-            dist[to[ed]] = dist[u]+1;
-            qu[qf++] = to[ed];
-        }
-    }
-
-    if (visi[src] != turn)
-        return INT_MAX;
-    return dist[src];
-}
-
-int dfs(int u) {
-    if (u == snk)
-        return 1;
-
-    for (int & ed = ht[u]; ed != -1; ed = nx[ed]) {
-        if (dist[u] != dist[to[ed]]+1 || us[ed])
+bool dfs (int v) {
+    for (int ed = hd[v]; ed != -1; ed = nx[ed]) {
+        int u = to[ed];
+        if (vis[u] == turn)
             continue;
-        if (dfs(to[ed])) {
-            us[ed] = 1;
-            us[ed^1] = 0;
+        vis[u] = turn;
+
+        if (mat[u] == -1 || dfs(mat[u])) {
+            mat[u] = v;
             return 1;
         }
     }
@@ -62,42 +28,39 @@ int dfs(int u) {
     return 0;
 }
 
-int dinic() {
-    int res = 0;
-    while (bfs() != INT_MAX) {
-        for (int i = 0; i <= snk; i++)
-            ht[i] = hd[i];
-        while (dfs(src))
-            res++;
-    }
-    return res;
-}
-
 int main () {
+    int tt;
     scanf("%d", &tt);
+
     for (int ts = 1; ts <= tt; ts++) {
-        memset(hd, -1, sizeof hd);
-
         scanf("%d", &n);
-        for (int i =0 ; i < n; i++)
+        for (int i = 0; i < n; i++) {
             scanf("%d", a+i);
+            hd[i] = -1;
+        }
+
         scanf("%d", &m);
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < m; i++) {
             scanf("%d", b+i);
+            mat[i] = -1;
+            vis[i] = 0;
+        }
 
-        src = n+m;
-        snk = src+1;
+        es = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (b[j] != 0 && (a[i] == 0 || b[j]%a[i]))
+                    continue;
+                nx[es] = hd[i]; hd[i] = es; to[es] = j; es++;
+            }
+        }
 
-        for (int i = 0; i < n; i++)
-            connect(src, i);
-        for (int i = 0; i < m; i++)
-            connect(i+n, snk);
+        int tot = 0;
+        for (int i = 0; i < n; i++) {
+            turn++;
+            tot += dfs(i);
+        }
 
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                if (a[i] != 0 && b[j]%a[i] == 0)
-                    connect(i, j+n);
-
-        printf("Case %d: %d\n", ts, dinic());
+        printf("Case %d: %d\n", ts, tot);
     }
 }
