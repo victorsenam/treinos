bebc558b582eddde41bfa8f12aea2d8b9a7c9033
Victor Sena Molero <victorsenam@gmail.com>
2015-12-28 14:27:38 -0200

    Cats Transport WA

--- victorsenam/ojs/codeforces/311/B/B.cpp
+++ victorsenam/ojs/codeforces/311/B/B.cpp
@@ -1,4 +1,4 @@
-// JEITO NOOB PROVAVELMENTE CERTO
+// WA TESTE 2
 
 #include <bits/stdc++.h>
 
@@ -19,22 +19,34 @@ const int P = 102;
 int n, m, p;
 num d[N];
 num t[N];
-num dp[N][P];
+num dp[P][N];
 num pre[N];
+
+num sa[N];
+num sb[N];
+num st[N];
+int ss;
+
+num inter (int i) {
+    if (!i)
+        return INT_MIN;
+
+    return (sb[i]-sb[i-1]+sa[i-1]-sa[i]-1)/(sa[i-1]-sa[i]);
+}
 
 int main () {
     scanf("%d %d %d", &n, &m, &p);
 
     d[0] = 0;
     for (int i = 1; i < n; i++) {
-        scanf("%lld", d+i);
+        scanf("%I64d", d+i);
         d[i] += d[i-1];
     }
 
     pre[0] = 0;
     for (int i = 0; i < m; i++) {
         int h;
-        scanf("%d %lld", &h, t+i);
+        scanf("%d %I64d", &h, t+i);
         t[i] -= d[h-1];
     }
 
@@ -42,21 +54,28 @@ int main () {
 
     for (int i = 0; i < m; i++) {
         pre[i] = pre[i-!!i] + t[i];
-        dp[i][1] = t[i]*num(i+1) - pre[i];
-
-        printf("%lld ", dp[i][1]);
+        dp[1][i] = t[i]*num(i+1) - pre[i];
     }
-    printf("\n");
 
-    for (int i = 0; i < m; i++) {
-        for (int k = 2; k <= p; k++) {
-            dp[i][k] = dp[i][k-1];
-
-            for (int j = 0; j < i; j++) {
-                dp[i][k] = min(dp[i][k], dp[j][k-1] + t[i]*num(i-j) - pre[i] + pre[j-!!j]);
+    for (int k = 2; k <= p; k++) {
+        ss = 0;
+        for (int i = 0; i < m; i++) {
+            sa[ss] = -i;
+            sb[ss] = dp[k-1][i] + pre[i-!!i];
+            st[ss] = inter(ss);
+            while (ss && st[ss] <= st[ss-1]) {
+                swap(sa[ss], sa[ss-1]);
+                swap(sb[ss], sb[ss-1]);
+                ss--;
+                st[ss] = inter(ss);
             }
+            ss++;
+
+            int l = upper_bound(st, st+ss, t[i]) - st - 1;
+
+            dp[k][i] = sa[l]*t[i] + sb[l] + t[i]*i - pre[i];
         }
     }
 
-   printf("%lld\n", dp[m-1][p]);
+   printf("%I64d\n", dp[p][m-1]);
 }
