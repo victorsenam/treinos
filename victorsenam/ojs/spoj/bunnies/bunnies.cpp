9130c05daf8f843e8a726ae1076e4a774bace6ae
Victor Sena Molero <victorsenam@gmail.com>
2016-01-09 01:02:28 -0200

    Cvx trick is inverted for this problem, but perfect for many others

--- victorsenam/ojs/spoj/bunnies/bunnies.cpp
+++ victorsenam/ojs/spoj/bunnies/bunnies.cpp
@@ -6,7 +6,6 @@ typedef long long int ll;
 typedef ll num;
 
 const int N = 3007;
-const num INF = LLONG_MAX - 1000000000ll;
 
 #ifndef ONLINE_JUDGE
 #define DEBUG(...) {fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n");}
@@ -14,38 +13,48 @@ const num INF = LLONG_MAX - 1000000000ll;
 #define DEBUG(...) {}
 #endif
 
+struct cvx {
+    num sa[N], sb[N], st[N];
+    int ss;
+
+    inline num numceil (num a, num b)
+    { return (a+b-1)/b; }
+
+    inline num inter (int i) {
+        if (!i)
+            return LLONG_MIN;
+
+        return numceil(sb[i]-sb[i-1], sa[i-1]-sa[i]);
+    }
+
+    void insert (num a, num b) {
+        sa[ss] = a;
+        sb[ss] = b;
+        st[ss] = inter(ss);
+
+        while (ss && st[ss] <= st[ss-1]) {
+            sa[ss-1] = sa[ss];
+            sb[ss-1] = sb[ss];
+            ss--;
+            st[ss] = inter(ss);
+        }
+        ss++;
+    }
+
+    inline void reset ()
+    { ss = 0; }
+
+    num get (num x) {
+        int l = upper_bound(st, st+ss, x) - st - 1;
+        return sa[l]*x + sb[l];
+    }
+};
+
 num dp[N][N];
-int opt[N][N];
 int turn;
 int k, n, t;
-num a[N];
-
-void solve (int qk, int qn) {
-    for (int i = 1; i <= qk; i++) {
-        dp[i][i] = dp[i-1][i-1] + a[i-1];
-        opt[i][i] = i;
-    }
-
-    for (int t = 1; t <= qn; t++) {
-        for (int k = 1; k <= qk && k + t <= qn; k++) {
-            int st = opt[k][k+t-1];
-            dp[k][k+t] = dp[k-(a[st-1]==a[k+t-1])][k+t-1] + a[st-1];
-            opt[k][k+t] = st;
-
-            int en = k+t;
-            if (k < qk)
-                en = opt[k+1][k+t];
-
-            for (int j = st+1; j <= en; j++) {
-                num val = dp[k-(a[j-1]==a[k+t-1])][k+t-1] + a[j-1];
-                if (val < dp[k][k+t]) {
-                    dp[k][k+t] = val;
-                    opt[k][k+t] = j;
-                }
-            }
-        }
-    }
-}
+num v[N];
+cvx trick;
 
 int main () {
     scanf("%d", &t);
@@ -54,11 +63,27 @@ int main () {
         scanf("%d %d", &n, &k);
 
         for (int i = 0; i < n; i++)
-            scanf("%lld", a+i);
+            scanf("%lld", v+i);
 
-        sort(a, a+n);
+        sort(v, v+n);
 
-        solve(k, n);
+        for (int i = 0; i <= n; i++)
+            dp[0][i] = 0;
+
+        for (int p = 1; p <= k; p++) {
+            trick.reset();
+
+            for (int i = p; i <= n; i++) {
+                trick.insert(v[i-1], dp[p-1][i-1] - (i-1)*v[i-1]);
+                dp[p][i] = trick.get(i);
+                for (int j = p-1; j < i; j++) {
+                    if (dp[p][i] > (i-j)*v[j] + dp[p-1][j]) {
+                        DEBUG("Fail: [%d] : %lld", j, (i-j)*v[j] + dp[p-1][j]);
+                    }
+                }
+                DEBUG("(%d,%d) : %lld", p, i, dp[p][i]);
+            }
+        }
 
         printf("%lld\n", dp[k][n]);
     }
