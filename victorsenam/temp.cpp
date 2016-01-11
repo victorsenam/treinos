d4c72e258c263f58543f912000b6bd1761dfa4a8
Victor Sena Molero <victorsenam@gmail.com>
2016-01-11 12:49:36 -0200

    Mudando template

--- victorsenam/temp.cpp
+++ victorsenam/temp.cpp
@@ -6,7 +6,7 @@ typedef long long int ll;
 typedef int num;
 
 #ifndef ONLINE_JUDGE
-#define DEBUG(...) {fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n");}
+#define DEBUG(...) {fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__);}
 #else
 #define DEBUG(...) {}
 #endif
