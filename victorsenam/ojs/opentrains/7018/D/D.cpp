c214cbeef44d2fb8580a54d12abeb0f4587cdded
Victor Sena <victorsenam@gmail.com>
2016-02-16 15:42:00 -0200

    tananananana

--- victorsenam/ojs/opentrains/7018/D/D.cpp
+++ victorsenam/ojs/opentrains/7018/D/D.cpp
@@ -2,92 +2,42 @@
 
 using namespace std;
 
-const int N = 107;
-const int M = 407;
-const int C = 2007;
+const int N = 307;
+typedef long long int ll;
 
-struct res {
-    int path;
-    int length;
-};
-
-int chpts, cfpts, m, dst, en;
-int hd[N], nx[M], pv[M], to[M], cf[M], wg[M], deg[M], es;
+int hd[N], to[N], nx[N], wg[N], cf[N], es;
 bool ch[N];
-res memo[M][C];
-int visi[M][C];
-
-inline void bef (int ed, int nw) {
-    nx[pv[ed]] = nw;
-    pv[nw] = pv[ed];
-
-    pv[ed] = nw;
-    nx[nw] = ed;
-}
-
-void insert (int u, int nw) {
-    if (!hd[u]) {
-        hd[u] = nx[nw] = pv[nw] = nw;
-    } else if (deg[nw] < deg[hd[u]]) {
-        bef(hd[u], nw);
-        hd[u] = nw;
-    } else {
-        int ed = nx[hd[u]];
-        while (ed != hd[u] && deg[ed] < deg[nw])
-            ed = nx[ed];
-        bef(ed, nw);
-    }
-}
+int st[N], ss;
+int turn, us[N];
+ll sim, ans, att;
+int nch, m, ncf, dist, end;
+int ts;
 
 int main () {
-    while (scanf("%d %d %d %d %d %d %d", &chpts, &m, &cfpts, &dst, to+1, &en, deg) && chpts+m+cfpts+dst+to[1]+en+deg[1]) {
-        memset(hd, 0, sizeof hd);
-        memset(ch, 0, sizeof ch);
-        es = 2;
-        wg[0] = wg[1] = 0;
+    ts = 0;
+    while (scanf("%d %d %d %d %d %d %d", &nch, &m, &ncf, &dist, to+1, &end, deg) && end) {
+        to[1]--;
+        deg[0] = (deg[0]+180)%360;
 
-        for (int i = 0; i < chpts; i++) {
-            int a;
-            scanf("%d", &a); a--;
-            ch[a] = 1;
-        }
+        es = 2;
+        ss = 0;
+
+        memset(ch, 0, sizeof ch);
+        memset(cf, 0, sizeof cf);
 
         for (int i = 0; i < m; i++) {
-            scanf("%d %d %d %d %d", to+es+1, to+es, deg+es, deg+es+1, wg+es);
-            wg[es] = wg[es+1];
-            cf[es] = cf[es+1] = 0;
-            to[es]--; to[es+1]--;
+            scanf("%d %d %d %d %d", to+es, to+es+1, deg+es+1, deg+es);
+            deg[es+1] = deg[es];
 
-            // tinha entendido errado, n precisa ordenar, mas vou deixar aq pq ta sexy...
-            insert(to[es+1], es);
-            insert(to[es], es+1);
-            
+            nx[es] = hd[to[es+1]]; hd[to[es+1]] = es;
+            nx[es+1] = hd[to[es]]; hd[to[es]] = es+1;
+
             es += 2;
         }
-        
-        for (int i = 0; i < cfpts; i++) {
-            int vt, ed, ds;
-            scanf("%d %d %d", &vt, &ed, &ds);
-            vt--;
 
-            ed <<= 1;
-            if (to[ed] == vt)
-                ed++;
+        st[ss++] = 1;
 
-            cf[ed] = max(cf[ed], ds);
-            cf[ed^1] = max(cf[ed^1], wg[es]-ds);
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (!hd[i])
-                continue;
-            printf("%d:", i+1);
-            int ed = hd[i];
-            do {
-                printf(" %d(%d)", to[ed]+1, deg[ed]);
-                ed = nx[ed];
-            } while (ed != hd[i]);
-            printf("\n");
+        while (att || st[ss-1] != end) {
         }
     }
 }
