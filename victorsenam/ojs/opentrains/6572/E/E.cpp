fdeab194b4fda34b01e00bc6076931f0ed6cd89c
Victor Sena Molero <victorsenam@gmail.com>
2016-02-24 17:49:33 -0300

    Imagina que eu tenho um torneio :)

--- victorsenam/ojs/opentrains/6572/E/E.cpp
+++ victorsenam/ojs/opentrains/6572/E/E.cpp
@@ -2,81 +2,61 @@
 
 using namespace std;
 typedef long long int ll;
+const int N = 2000007;
+const ll MOD = 1000000007ll;
 
-const int K = 19;
-const int N = (1<<K);
-const ll MOD = 1000000007;
+inline ll mod (ll a) { return a%MOD; }
 
-struct num {
-    int x;
-    num (ll v) : x(v) {}
-    num (int v) : x(v) {}
-    num () {}
-    void operator += (num y)
-    { x = (ll(x)+ll(y.x))%MOD; }
-    void operator *= (num y)
-    { x = (ll(x)*ll(y.x))%MOD; }
-    void operator -= (num y)
-    { x = ((ll(x)-ll(y.x))%MOD+MOD)%MOD; }
-    num fexp (ll e) {
-        num r(1ll), a(x);
-        while (e) {
-            if (e&1)
-                r *= a;
-            a *= a;
-            e >>= 1;
-        }
-        return r;
-    }
-    void operator /= (num y)
-    { *this *= y.fexp(MOD-2); }
-};
-
-num ans, loc;
-num fat[N];
-num p[N], a[N];
-num choose (int n, int k) {
-    if (k < 0 || n < k) return num(0);
-    num r = fat[n]; num q = fat[k];
-    q *= fat[n-k]; r /= q;
-
-    printf("(%d %d) = %d\n", n, k, r.x);
+ll fexp (ll a, int e) {
+    ll r;
+    for (r = 1ll; e; e >>= 1, a = mod(a*a)) if (e&1) r = mod(r*a);
     return r;
 }
 
+
+ll a[N], fat[N], ifat[N];
+ll m, n;
+ll res, qt;
+ll f, g;
+int k;
+
+inline ll choose (ll n, ll p) {
+    if (p > n || p < 0) return 0;
+    return mod(mod(fat[n]*ifat[p])*ifat[n-p]);
+}
+inline ll ichoose (ll n, ll p) {
+    if (p > n || p < 0) return 0;
+    return mod(mod(ifat[n]*fat[p])*fat[n-p]);
+}
+
 int main () {
-    int k;
+    fat[0] = 1ll;
+    for (int i = 1; i < N; i++) {
+        fat[i] = mod(fat[i-1]*i);
+    }
+    ifat[N-1] = fexp(fat[N-1], MOD-2);
+    for (int i = N-2; i >= 0; i--)
+        ifat[i] = mod(ifat[i+1]*(i+1));
+
     scanf("%d", &k);
-    num ans(0ll);
+    n = (1ll << k);
+    
+    for (int i = 1; i <= n; i++)
+        scanf("%lld", a+i);
 
-    fat[0] = num(1);
-    for (int i = 1; i <= (1<<k); i++) {
-        fat[i] = fat[i-1];
-        fat[i] *= num(i);
-    }
-
-    for (int i = 0; i < (1<<k); i++)
-        scanf("%lld", &a[i].x);
-
-    for (int j = 1; j <= k; j++) {
-        num loc(0ll), tp(0ll);
-        for (int i = (1<<(k-j))-1; i < (1<<k); i++) {
-            p[i] = choose(i+1, (1<<(k-j)));
-            p[i] /= choose((1<<k), (1<<(k-j)));
-            p[i] *= a[i];
-            tp += p[i];
+    res = 0ll;
+    qt = n;
+    for (ll m = 1ll; m <= n; (m<<=1), (qt>>=1)) {
+        g = 0;
+        for (int i = 2; i <= n; i++) {
+            g = mod(g + mod(a[i-1]*mod(choose(n-i+1-m, m-1)*ichoose(n-m, m))));
+            f = mod(a[i]*mod(choose(n-i, m-1)*ichoose(n,m)));
+            res = mod(res + mod(qt*mod(f*g)));
+            /*for (int j = 1; j+m+m-1 <= n && j < i; j++) {
+                printf("%lld %lld -> (%lld %lld) %lld (%lld %lld) %lld\n", a[i], a[j], n-i, m-1,  mod(choose(n-i, m-1)*ichoose(n,m)), n-j-m, m-1, mod(choose(n-j-m, m-1)*ichoose(n-m,m)));
+                res = mod(res + mod(qt*mod(mod(mod(a[i]*a[j])*mod(choose(n-i, m-1)*ichoose(n,m)))*mod(choose(n-j-m, m-1)*ichoose(n-m,m)))));
+            }*/
         }
-        for (int i = (1<<(k-j))-1; i < (1<<k); i++) {
-            num aux = tp;
-            aux -= p[i];
-            printf("%d(%d) %d*%d = ", i, a[i], p[i], aux);
-            aux *= p[i];
-            printf("%d\n", aux);
-            loc += aux;
-        }
-        loc *= num(1ll<<(j-1));
-        ans += loc;
-        printf("%d -> %d\n", j, loc.x);
     }
-    printf("%d\n", ans.x);
+    printf("%lld\n", res);
 }
