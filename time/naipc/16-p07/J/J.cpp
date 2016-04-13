b6acd2fcad73f80bc160a5d9b759da8ec2bd7c0b
Victor Sena Molero <victorsenam@gmail.com>
2016-04-13 20:01:42 -0300

    alalala

--- time/naipc/16-p07/J/J.cpp
+++ time/naipc/16-p07/J/J.cpp
@@ -6,7 +6,7 @@ typedef long long int ll;
 
 #define ONLINE_JUDGE
 #ifndef ONLINE_JUDGE
-#define DEBUG(...) {fprintf(stderr, __VA_ARGS__);}
+#define DEBUG(...) {fprintf(stdout, __VA_ARGS__);}
 #else
 #define DEBUG(...) {}
 #endif
@@ -14,80 +14,75 @@ typedef long long int ll;
 const int N = 60;
 const int M = 50000;
 int n;
-int p[N];
 int hd[N], nx[M], to[M], pr[M], es;
 
-bool cmp (int cr, int nw, int keep) {
-    if (to[hd[cr]] == to[hd[nw]]) {
-        if (cr == keep) return 0;
-        if (nw == keep) return 1;
-        if (pr[hd[cr]] < pr[hd[nw]]) return 0;
-        return 1;
-    }
-    
-    return to[hd[nw]] > to[hd[cr]];
+bool cmp_2 (int i, int j) {
+    if (i == j) return 1;
+    if (to[i] == to[j]) return cmp_2(pr[i], pr[j]);
+    return to[i] <= to[j];
+}
+
+inline bool cmp (int i, int j, int mx) {
+    if (i == j) return 1;
+    if (to[hd[i]] == to[hd[j]]) {
+        if (i == mx) return 1;
+        if (j == mx) return 0;
+      return cmp_2(pr[hd[i]], pr[hd[j]]);
+    return (to[nx[hd[i]]] <= to[nx[hd[j]]]);
+}
+return to[hd[i]] > to[hd[j]];
 }
 
 int main () {
-    int turn = 0;
-    while (scanf("%d", &n) != EOF) {
-        memset(hd, 0, sizeof hd);
-        memset(pr, 0, sizeof pr);
-        memset(to, 0, sizeof to);
-        es = 2;
+int turn = 0;
+while (scanf("%d", &n) != EOF) {
+    memset(hd, 0, sizeof hd);
+    memset(nx, 0, sizeof nx);
+    memset(to, 0, sizeof to);
+    memset(pr, 0, sizeof pr);
+    es = 2;
 
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            int h;
-            scanf("%d", &h);
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        int h;
+        scanf("%d", &h);
+    //    DEBUG("stack %d\n", i);
+        for (int j = 0; j < h; j++) {
+            scanf("%d", to+es);
+            nx[es] = hd[i]; hd[i] = es;
+            if (to[nx[es]] == to[es])
+                pr[es] = pr[nx[es]];
+            else
+                pr[es] = nx[es];
 
-            for (int j = 0; j < h; j++) {
-                scanf("%d", to+es);
-                nx[es] = hd[i]; hd[i] = es; es++;
-                if (to[nx[es]] == to[es])
-                    pr[es] = pr[nx[es]];
-                else
-                    pr[es] = to[nx[es]];
-                cnt++;
-            }
+  //          DEBUG("%d(%d)->%d(%d)\n", es, to[es], pr[es], to[pr[es]]);
+            cnt++;
+            es++;
         }
-
-        int att = n;
-        /*
-        for (int i = 1; i < n; i++) {
-            if (to[hd[i]] == to[hd[att]]) {
-                if ((!nx[hd[att]]) || (nx[hd[i]] && to[nx[hd[i]]] > to[nx[hd[att]]]))
-                    att = i;
-            } else if (to[hd[i]] > to[hd[att]]) {
-                att = i;
-            }
-        }
-        */
-        
-        int res = -1;
-        while (cnt) {
-            int maxi = att;
-
-            for (int i = 0; i < n; i++) {
-                if (cmp(maxi, i, att))
-                    maxi = i;
-            }
-            DEBUG("%d(%d)", maxi, to[hd[maxi]]);
-
-            if (maxi != att) {
-                res++;
-                DEBUG("+");
-                if (hd[att]) {
-                    res++;
-                    DEBUG("+");
-                }
-            }
-            DEBUG("\n");
-            hd[maxi] = nx[hd[maxi]];
-            att = maxi;
-            cnt--;
-        }
-
-        printf("Case %d: %d\n", ++turn, res);
     }
+
+    int att = n;
+    
+    int res = -1;
+    while (cnt) {
+        int maxi = att;
+
+        for (int i = 0; i < n; i++)
+            if (!cmp(maxi, i, att))
+                maxi = i;
+
+        if (maxi != att) {
+            res++;
+            if (hd[att])
+                res++;
+        }
+
+        DEBUG("%d -> %d\n", maxi, res);
+        hd[maxi] = nx[hd[maxi]];
+        att = maxi;
+        cnt--;
+    }
+
+    printf("Case %d: %d\n", ++turn, res);
+}
 }
---
+++ victorsenam/ojs/icpc/4287/4287.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef unsigned long long int ull;
+typedef long long int ll;
+
+#ifndef ONLINE_JUDGE
+#define DEBUG(...) {fprintf(stderr, __VA_ARGS__);}
+#else
+#define DEBUG(...) {}
+#endif
+
+const int N = 40000;
+
+int t;
+int n, m;
+int ind[N], out[N];
+
+int main () {
+}
