af7515e6b0e2720f0cb656a2b60e2440ce5301eb
Victor Sena Molero <victorsenam@gmail.com>
2016-06-06 20:46:29 -0300

    lalala

--- victorsenam/code_temp.cpp
+++ victorsenam/code_temp.cpp
@@ -5,8 +5,10 @@ typedef unsigned long long int ull;
 typedef long long int ll;
 
 #ifndef ONLINE_JUDGE
+#define LLD "%lld"
 #define DEBUG(...) {fprintf(stderr, __VA_ARGS__);}
 #else
+#define LLD "%I64d"
 #define DEBUG(...) {}
 #endif
 
