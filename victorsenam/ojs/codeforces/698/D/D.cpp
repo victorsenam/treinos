0a1f8e3fbfdd9b183d0bfdcf96e9a8afd991355e
Victor Sena Molero <victorsenam@gmail.com>
2016-07-19 21:12:19 -0300

    Nurikabe fail, 698 D fail

--- victorsenam/ojs/codeforces/698/D/D.cpp
+++ victorsenam/ojs/codeforces/698/D/D.cpp
@@ -30,9 +30,14 @@ struct point {
         p.x = x;
         p.y = y;
         ll res = (p-a).cross(p-b);
-        if (res == 0)
-            return -(a.x<x) + (b.x<x);
         return res;
+    }
+
+    inline bool islin (point a, point b) {
+        point p;
+        p.x = x; p.y = y;
+        if ((a.x>p.x)^(b.x>p.x)) return 0;
+        return (p.cmpang(a, b) == 0);
     }
 
     point operator - (point ot) {
@@ -47,16 +52,20 @@ int pos[K][N];
 int p[K][N];
 point a[K];
 point m[N];
+int ch[K][K];
 int memo[K][N][L];
 int piv;
 int n, k;
 int can[N];
 
 bool cmp_t (int i, int j) {
-    if (a[piv].cmpang(m[i], m[j]) == 0) {
-        return a[piv].dist(m[i]) < a[piv].dist(m[j]);
-    }
-    return a[piv].cmpang(m[i], m[j]) < 0;
+    if ((m[i].x>a[piv].x)^(m[j].x>a[piv].x))
+        return m[i].x < m[j].x;
+
+    if (a[piv].cmpang(m[i], m[j]) != 0)
+        return a[piv].cmpang(m[i], m[j]) < 0;
+
+    return a[piv].dist(m[i]) < a[piv].dist(m[j]);
 }
 
 int buildfrom (int a, int b) {
@@ -74,23 +83,33 @@ int buildfrom (int a, int b) {
 }
 
 int pd (int j, int i, int msk) {
-    int ps = pos[j][i];
-    if (ps == 0) return 1;
-    int prev = p[j][ps-1];
-    if (a[j].cmpang(m[prev], m[i]) != 0) return 1;
-
     int & me = memo[j][i][msk];
     if (me != 0) return (me > 0);
     
     me = 0;
-    for (int mask = 1; !me && (mask < (1<<__builtin_popcount(msk))); mask++) {
-        int nm = buildfrom(msk, mask);
-        assert((nm&msk) == nm);
-        for (int r = 0; !me && r < k; r++)
-            if ((1<<r)&nm)
-                me |= (pd(r, prev, (nm&(~(1<<r)))) && pd(j, prev, (msk&(~nm))));
+
+    int ps = pos[j][i];
+    if (ps == 0) me = 1;
+    if (!me) {
+        int prev = p[j][ps-1];
+        if (!a[j].islin(m[prev], m[i])) me = 1;
+
+        for (int mask = 1; !me && (mask < (1<<__builtin_popcount(msk))); mask++) {
+            int nm = buildfrom(msk, mask);
+            assert((nm&msk) == nm);
+            for (int r = 0; !me && r < k; r++) {
+                if ((1<<r)&nm)
+                    me |= (pd(r, prev, (nm&(~(1<<r)))) && pd(j, prev, (msk&(~nm))));
+            }
+        }
+
+        for (int r = 0; !me && r < k; r++) {
+            if ((1<<r)&msk && m[prev].islin(a[j], a[r]) && !ch[j][r])
+                me |= (pd(r, prev, (msk&(~(1<<r)))));
+        }
     }
     if (me == 0) me--;
+    //DEBUG("%d %d %s -> %d\n", j, i, bitset<3>(msk).to_string().c_str(), me > 0);
     return (me > 0);
 }
 
@@ -110,15 +129,23 @@ int main () {
         for (int i = 0; i < n; i++)
             p[j][i] = i;
 
-        DEBUG("from (%lld,%lld)\n", a[j].x, a[j].y);
+        //DEBUG("from (%lld,%lld)\n", a[j].x, a[j].y);
 
         piv = j;
         sort(p[j], p[j] + n, cmp_t);
         for (int i = 0; i < n; i++) {
-            DEBUG("%d : (%lld,%lld)\n", i, m[p[j][i]].x, m[p[j][i]].y);
-            if (i && a[j].cmpang(m[p[j][i-1]],m[p[j][i]]))
-                DEBUG("-\n");
+            //if (i && !a[j].islin(m[p[j][i-1]],m[p[j][i]]))
+                //DEBUG("-\n");
+            //DEBUG("%d : (%lld,%lld)\n", i, m[p[j][i]].x, m[p[j][i]].y);
             pos[j][p[j][i]] = i;
+        }
+    }
+    for (int i = 0; i < k; i++) {
+        for (int j = i+1; j < k; j++)  {
+            for (int mi = 0; mi < n; mi++)
+                if (a[i].islin(m[mi], a[j]) && a[j].islin(m[mi], a[i]))
+                    ch[i][j] = 1;
+            ch[i][j] = ch[j][i];
         }
     }
 
@@ -131,5 +158,8 @@ int main () {
             DEBUG("(%lld,%lld)\n", m[i].x, m[i].y);
         }
     }
+
+
+ //   DEBUG("%d\n", pd(1, 3, (msk&(~2))));
     printf("%d\n", cnt);
 }
--- victorsenam/ojs/icpc/4103/4103.cpp
+++ victorsenam/ojs/icpc/4103/4103.cpp
@@ -5,227 +5,219 @@ typedef unsigned long long int ull;
 typedef long long int ll;
 
 #define DEBUG(...) {fprintf(stderr, __VA_ARGS__);}
-//#define DEBUG(...) {}
 
-const int N = 12;
+const int N = 10;
 const int mv[4][2] = {
-    {0, 1},
     {1, 0},
-    {0, -1},
-    {-1, 0}
+    {0, 1},
+    {-1, 0},
+    {0, -1}
 };
 
-int n, m;
+
 char mat[N][N];
 int val[N][N];
+ll visi[N][N];
 int qt;
+int n, m;
+ll turn;
 
 struct point {
     int i, j;
 
+    point () : i(0), j(0) {}
+    point (int x, int y) : i(x), j(y) {}
+
+    int & val ()
+    { return ::val[i][j]; }
+    char & mat ()
+    { return ::mat[i][j]; }
+    bool isval ()
+    { return i >= 0 && i < n && j >= 0 && j < m; }
+
     bool operator < (const point & ot) const {
-        if (i == ot.i)
-            return j < ot.j;
-        return i < ot.i;
-    }
-
-    int & val () {
-        return ::val[i][j];
-    }
-
-    char & mat () {
-        return ::mat[i][j];
+        if (ot.i != i) return i < ot.i;
+        return j < ot.j;
     }
 
     bool isblank () {
-        point q;
-        if (val() > 0) return 0;
+        if (!isval() || val() > 0) return 0;
         for (int k = 0; k < 4; k++) {
-            q.i = i + mv[k][0];
-            q.j = i + mv[k][1];
-            if (q.isval() && q.val() > 0) return 1;
+            point p(i+mv[k][0],j+mv[k][1]);
+            if (p.isval() && p.val() > 0)
+                return 1;
         }
         return 0;
     }
 
-    bool isval () {
-        return i >= 0 && i < n && j >= 0 && j < m;
-    }
-
-    bool check_blanks () {
+    bool blankok () {
         if (!isblank()) return 1;
-        point p;
-        p.i = i; p.j = j;
-        point q;
-
         for (int k = 0; k < 4; k++) {
             bool ok = 0;
             for (int l = 0; !ok && l < 2; l++) {
+                point p(i+mv[(k+l)%4][0], j+mv[(k+l)%4][1]);
+                if (!p.isblank())
+                    ok = 1;
+            }
+            point p(i+mv[k][0]+mv[(k+1)%4][0], j+mv[k][1]+mv[(k+1)%4][1]);
+            if (!p.isblank())
                 ok = 1;
-                q = p;
-                q.i += mv[(k+l)%4][0];
-                q.j += mv[(k+l)%4][1];
-                if (!q.isval() || !q.isblank()) ok = 1;
-            }
-            
-            q = p;
-            for (int l = 0; !ok && l < 2; l++) {
-                q.i += mv[(k+l)%4][0];
-                q.j += mv[(k+l)%4][1];
-            }
-            if (!q.isval() || !q.isblank()) ok = 1;
-
-            if (!ok) return 0;
+            if (!ok)
+                return 0;
         }
         return 1;
     }
 
-    bool put (vector<point> v) {
-        point p;
-        DEBUG("try at (%d,%d) -> %d\n", i, j, (int) v.size());
+    bool put (vector<point> v, int sh) {
         qt++;
-        for (int l = 0; l < v.size(); l++) {
-            v[l].i += i;
-            v[l].j += j;
-            //DEBUG("++(%d,%d)\n", v[l].i, v[l].j);
-
-            if (!v[l].isval() || v[l].isblank() || v[l].val() > 0) return 0;
-            continue;
-            for (int k = 0; k < 4; k++) {
-                p = v[l];
-                p.i += mv[k][0];
-                p.j += mv[k][1];
-                if (p.isval() && p.val() > 0) return 0;
-            }
+        for (point q : v) {
+            point p(i+q.i-v[sh].i, j+q.j-v[sh].j);
+            if (!p.isval() || p.val() != 0 || p.isblank()) 
+                return 0;
         }
-        //DEBUG("putting\n");
-        for (int l = 0; l < v.size(); l++)
-            v[l].val() = qt;
-        //DEBUG("check blanks\n");
-        DEBUG("ok\n");
-
-        for (int x = 0; x < n; x++) {
-            for (int y = 0; y < m; y++) {
-                DEBUG("%d ", ::val[x][y]);
-            }
-            DEBUG("\n");
+        for (point q : v) {
+            point p(i+q.i-v[sh].i, j+q.j-v[sh].j);
+            p.val() = qt;
         }
-
-        return 1;
-        for (int l = 0; l < v.size(); l++) {
-            //DEBUG("(%d,%d)\n", v[l].i, v[l].j);
+        for (point q : v) {
+            point p(i+q.i-v[sh].i, j+q.j-v[sh].j);
             for (int k = 0; k < 4; k++) {
-                p = v[l];
-                p.i += mv[k][0];
-                p.j += mv[k][1];
-                //DEBUG("->(%d,%d)\n", p.i, p.j);
-                if (p.isval() && !p.check_blanks()) return 0;
+                point c(p.i+mv[k][0], p.j+mv[k][1]);
+                if (!c.blankok())
+                    return 0;
             }
         }
         return 1;
     }
 
-    void unput (vector<point> v) {
-        //DEBUG("fail at (%d,%d)\n", i, j);
-        for (int l = 0; l < v.size(); l++) {
-            v[l].i += i;
-            v[l].j += j;
-
-            if (v[l].val() == qt)
-                v[l].val() = 0;
+    void unput (vector<point> v, int sh) {
+        for (point q : v) {
+            point p(i+q.i-v[sh].i, j+q.j-v[sh].j);
+            if (p.isval() && p.val() == qt)
+                p.val() = 0;
         }
         qt--;
     }
 };
 
+bool dfs (point p) {
+    if (!p.isval() || p.val() || visi[p.i][p.j] == turn) return 0;
+    visi[p.i][p.j] = turn;
+
+    bool res = p.isblank();
+    for (int k = 0; k < 4; k++) {
+        point q(p.i+mv[k][0], p.j+mv[k][1]);
+        res |= dfs(q);
+    }
+    return res;
+}
+
+bool iscon () {
+    turn++;
+    point p(0,0);
+    int cnt = 0;
+    for (p.i = 0; p.i < n; p.i++) {
+        for (p.j = 0; p.j < m; p.j++) {
+            cnt += dfs(p);
+            if (cnt > 1) return 0;
+        }
+    }
+    return 1;
+}
+
 set<vector<point> > s[N];
 
-void gen_kminos (int i) {
-    for (vector<point> v : s[i-1]) {
-        for (point u : v) {
-            for (int k = 0; k < 4; k++) {
-                u.i += mv[k][0];
-                u.j += mv[k][1];
-                if (binary_search(v.begin(), v.end(), u)) continue;
-                vector<point> nx = v;
-                nx.push_back(u);
-                for (int j = nx.size()-1; j > 0 && nx[j] < nx[j-1]; j--)
-                    swap(nx[j], nx[j-1]);
-                if (nx[0].i != nx[0].j || nx[0].i != 0) {
-                    for (int j = nx.size()-1; j >= 0; j--) {
-                        nx[j].i -= nx[0].i;
-                        nx[j].j -= nx[0].j;
+void gen (int sz) {
+    point p;
+    vector<point> v;
+    if (sz == 1) {
+        p.i = p.j = 0;
+        v.push_back(p);
+        s[sz].insert(v);
+    } else {
+        for (vector<point> at : s[sz-1]) {
+            for (int i = 0; i < at.size(); i++) {
+                for (int k = 0; k < 4; k++) {
+                    p = at[i];
+                    p.i += mv[k][0];
+                    p.j += mv[k][1];
+                    if (binary_search(at.begin(), at.end(), p)) continue;
+                    v = at;
+                    v.push_back(p);
+
+                    for (int j = v.size()-1; j > 0 && v[j] < v[j-1]; j--)
+                        swap(v[j], v[j-1]);
+
+                    for (int j = v.size()-1; j >= 0; j--) {
+                        v[j].i -= v[0].i;
+                        v[j].j -= v[0].j;
                     }
+                    s[sz].insert(v);
                 }
-                s[i].insert(nx);
             }
         }
     }
 }
 
 bool bt (point p) {
-    point q;
-    if (p.i == n) {
-        for (q.i = 0; q.i < n; q.i++) {
-            for (q.j = 0; q.j < m; q.j++) {
-                if (q.val() == 0) return 0;
-                if (!q.check_blanks()) return 0;
-            }
+    while (!p.isval() || p.mat() == '.') {
+        if (p.j == m) {
+            p.i++;
+            p.j = 0;
+        } else if (p.i == n) {
+            for (p.i = 0; p.i < n; p.i++)
+                for (p.j = 0; p.j < m; p.j++)
+                    if ((p.val() == 0 && (p.mat() != '.' || !p.isblank())) || !p.blankok())
+                        return 0;
+            return iscon();
+        } else {
+            p.j++;
         }
     }
-    if (p.j == m) {
-        p.j = 0;
-        p.i++;
-        return bt(p);
-    }
-    if (p.mat() == '.') {
-        p.j++;
-        return bt(p);
-    }
     
-    q = p;
-    q.j++;
     int sz = p.mat()-'0';
+    point nx = p;
+    nx.j++;
+    int cnt = 0;
+    //DEBUG("(%d,%d) -> %d\n", p.i, p.j, sz);
     for (vector<point> v : s[sz]) {
-        if (p.put(v) && bt(q)) return 1;
-        p.unput(v);
+        for (int j = 0; j < sz; j++) {
+            if (p.put(v, j) && iscon() && bt(nx))
+                return 1;
+            p.unput(v, j);
+        }
     }
     return 0;
 }
 
 int main () {
-    point p;
-    p.i = p.j = 0;
-    vector<point> v;
-    v.push_back(p);
-    s[1].insert(v);
-    for (int i = 1; i < 10; i++) {
-        gen_kminos(i);
-        DEBUG("gen %d\n", i);
-    }
-    
+    for (int i = 1; i < N; i++)
+        gen(i);
+
+    bool fs = 0;
+
     while (scanf("%d %d", &n, &m) != EOF && n) {
+        if (fs)
+            printf("\n");
+        fs = 1;
         qt = 0;
         for (int i = 0; i < n; i++) {
-            scanf(" %s", mat[i]);
             for (int j = 0; j < m; j++) {
+                scanf(" %c", &mat[i][j]);
                 val[i][j] = 0;
             }
         }
 
-        bt(p);
+        bt(point(0,0));
 
-        for (p.i = 0; p.i < n; p.i++) {
-            for (p.j = 0; p.j < m; p.j++) {
-                printf("%d ", p.val());
-                continue;
-                if (p.isblank())
-                    printf("#");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (val[i][j])
+                    printf("%c", mat[i][j]);
                 else
-                    printf("%c", p.mat());
+                    printf("#");
             }
             printf("\n");
         }
-        printf("\n");
     }
 }
