b02236d75226d2173c54e510348013fced5ec3dd
Victor Sena Molero <victorsenam@gmail.com>
2016-07-27 11:39:48 -0300

    MASQNUBEZA,PARSA

--- victorsenam/ojs/yandex/round2-2016/E/E.cpp
+++ victorsenam/ojs/yandex/round2-2016/E/E.cpp
@@ -4,80 +4,77 @@ using namespace std;
 typedef unsigned long long int ull;
 typedef long long int ll;
 
-#define DEBUG(...) {fprintf(stderr, __VA_ARGS__);}
 
-const int N = 200011;
-const int M = 2*N;
+const int N = (2e5)+7;
+const int M = 3*N;
+clock_t t;
 
 struct bit {
-    int n;
     ll v[N];
-
-    void add (int i, ll x) {
-        for (i += 3; i < n+3; i+=(i&-i))
+    
+    inline void add (ll i, ll x) {
+        for (i += 2; i < N; i+=(i&-i))
             v[i] += x;
     }
 
-    ll get (int i) {
+    inline ll get (ll i) {
         ll x = 0;
-        for (i += 3; i > 0; i-=(i&-i))
+        for (i += 2; i > 0; i-=(i&-i))
             x += v[i];
         return x;
     }
-
-    ll query (int l, int r) {
-        return get(r-1) - get(l-1);
-    }
 };
 
-ll memo[M];
-int visi[M];
-int hd[N], to[M], nx[M], es;
-ll val[M];
 int ord[N][2], os;
-int v[N];
-int pr[N];
-int n;
-vector<int> its[N];
+int li[N], hd[N], to[M], nx[M], es;
+int n, cm;
+int pr[M];
+int v[M];
 bit tree;
+ll qtd, cst[M];
+ll sum;
 
-ll pd (int fr) {
-    ll & me = memo[fr];
-    if (fr && visi[fr]) return me;
-    visi[fr] = 1;
+int visi[N];
+ll memo[N];
 
-    me = 0;
-    me += val[fr];
-    int u = to[fr];
-    for (int ed = hd[u]; ed; ed = nx[ed])
-        if ((fr|1) != (ed|1))
-            me += pd(ed);
+ll dp (int u) {
+    ll & me = memo[u];
+    if (visi[u]++) return me;
+
+    if (!u) me = sum;
+    else me = dp(to[pr[u]^1]) + cst[pr[u]^1] - cst[pr[u]];
+
     return me;
 }
 
 void dfs (int fr) {
+    //assert(os <= n);
+    //assert(!fr || (fr >= cm && fr < es));
     int u = to[fr];
+    //DEBUG("%d[%d]\n", u+1, fr);
+    pr[u] = fr;
     ord[u][0] = os++;
-    pr[u] = (fr^1);
-
     for (int ed = hd[u]; ed; ed = nx[ed]) {
-        if ((ed|1) == (fr|1)) continue;
-        dfs(ed);
+        if ((ed|1) != (fr|1))
+            dfs(ed);
     }
-
     ord[u][1] = os;
-    //DEBUG("%d [%d,%d] from %d(%d)\n", u, ord[u][0], ord[u][1], to[fr], fr);
 }
 
 int main () {
+    t = clock();
     scanf("%d", &n);
 
+    es = 2;
     for (int i = 0; i < n; i++) {
         scanf("%d", v+i);
-        its[v[i]].push_back(i);
+
+        nx[es] = li[v[i]]; to[es] = i; li[v[i]] = es++;
     }
 
-    es = 2;
+    es|=1;
+    es++;
+    cm = es;
     for (int i = 1; i < n; i++) {
         int a, b;
         scanf("%d %d", &a, &b);
@@ -87,51 +84,46 @@ int main () {
         nx[es] = hd[b]; to[es] = a; hd[b] = es++;
     }
 
+    to[0] = 0;
     os = 0;
-    to[1] = 0;
-    dfs(1);
+    dfs(0);
+    qtd = 0;
+    sum = 0;
 
-    tree.n = n+1;
-    ll qtd = 0;
 
-    for (int _i = n; _i > 0; _i--) {
-        for (int _j = 0; _j < its[_i].size(); _j++) {
-            int i = its[_i][_j];
+    for (int att = n; att >= 0; att--) {
+    //for (int att = 1; att <= n; att++) {
+        for (int ps = li[att]; ps; ps = nx[ps]) {
+            int u = to[ps];
+            int fr = pr[u]; 
 
-            ll sum = 0;
-            //DEBUG("item %d\n", i);
-            for (int ed = hd[i]; ed; ed = nx[ed]) {
-                if (ed != pr[i]) {
-                    val[ed] = tree.get(ord[to[ed]][1]-1) - tree.get(ord[to[ed]][0]-1);
-                    sum += val[ed];
-                    //DEBUG("%d->%d : %lld\n", to[ed^1], to[ed], val[ed]);
-                }
+            for (int ed = hd[u]; ed; ed = nx[ed]) {
+                if ((ed|1) == (fr|1)) continue;
+                sum += (cst[ed] = (tree.get(ord[to[ed]][1]-1) - tree.get(ord[to[ed]][0]-1)));
+                //DEBUG("%d->%d : %lld\n", to[ed^1]+1, to[ed]+1,  cst[ed]);
             }
-            if (pr[i]) {
-                val[pr[i]] = qtd - sum;
-                //DEBUG("%d->%d : %lld = %lld - %lld [%d]\n", to[pr[i]^1], to[pr[i]], val[pr[i]], qtd, sum, pr[i]);
-            } else {
-                //DEBUG("is root\n");
-            }
+
+            if (fr)
+                cst[fr^1] = qtd - (tree.get(ord[u][1]-1) - tree.get(ord[u][0]-1));
+            //DEBUG("%d->%d : %lld *\n", to[fr]+1, to[fr^1]+1,  cst[fr^1]);
         }
-        for (int _j = 0; _j < its[_i].size(); _j++) {
-            tree.add(ord[its[_i][_j]][0], 1);
+        for (int ps = li[att]; ps; ps = nx[ps]) {
             qtd++;
+            int u = to[ps];
+            tree.add(ord[u][0], 1);
         }
     }
-
 
     int res = 0;
-    ll vres = LLONG_MAX;
+    ll rval = LLONG_MAX;
     for (int i = 0; i < n; i++) {
-        to[0] = i;
-        ll att = pd(0);
-        //DEBUG("%d : %lld\n", i, att);
-        if (att < vres) {
-            vres = att;
+        ll loc = dp(i);
+        //DEBUG("%d : %lld\n", i+1, loc);
+        if (loc < rval) {
             res = i;
+            rval = loc;
         }
     }
 
-    printf("%d %lld\n", res+1, vres);
+    printf("%d %lld\n", res+1, rval);
 }
