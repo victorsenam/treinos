26aad02f3c7edbfc8405365990ca7d7c7c7c79b6
Victor Sena Molero <victorsenam@gmail.com>
2016-07-28 14:34:31 -0300

    mycommit

--- victorsenam/ojs/yandex/round2-2016/E/E.cpp
+++ victorsenam/ojs/yandex/round2-2016/E/E.cpp
@@ -1,35 +1,41 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#define GNU __gnu_pbds
 
 using namespace std;
 typedef unsigned long long int ull;
 typedef long long int ll;
-typedef pair<int, int> pii;
-namespace GNU { typedef tree<pii, null_type, less<pii>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; } 
-using GNU::ordered_set;
 
 #define DEBUG(...) {fprintf(stderr, __VA_ARGS__);}
 
-const int N = 200007;
+const int N = 200011;
 const int M = 2*N;
 
 ll memo[M];
 int visi[M];
+int seen[N];
 int hd[N], to[M], nx[M], es;
 ll val[M];
+int ord[N][2], os;
 int v[N];
-ordered_set s[N], gen;
-int p[N];
 int pr[N];
 int n;
-int deg[N];
-int qu[N], qi, qf;
+vector<int> its[N];
+int bit[N];
+
+void add (int i, ll x) {
+    for (i += 3; i < n+3; i+=(i&-i))
+        bit[i] += x;
+}
+
+ll get (int i) {
+    ll x = 0;
+    for (i += 3; i > 0; i-=(i&-i))
+        x += bit[i];
+    return x;
+}
 
 ll pd (int fr) {
     ll & me = memo[fr];
-    if (fr && visi[fr]) return me;
+    if (visi[fr]) return me;
     visi[fr] = 1;
 
     me = 0;
@@ -41,13 +47,20 @@ ll pd (int fr) {
     return me;
 }
 
-void join (int a, int b) {
-    if (s[p[a]].size() < s[p[b]].size())
-        swap(p[a], p[b]);
+void dfs (int fr) {
+    int u = to[fr];
+    seen[u] = 1;
+    ord[u][0] = os++;
+    pr[u] = (fr^1);
 
-    for (pii aux : s[p[b]])
-        s[p[a]].insert(aux);
-    p[b] = p[a];
+    for (int ed = hd[u]; ed; ed = nx[ed]) {
+        if ((ed|1) == (fr|1)) continue;
+        //assert(to[ed] != pr[u]);
+        dfs(ed);
+    }
+
+    ord[u][1] = os;
+    //DEBUG("%d [%d,%d] from %d(%d)\n", u, ord[u][0], ord[u][1], to[fr], fr);
 }
 
 int main () {
@@ -55,10 +68,7 @@ int main () {
 
     for (int i = 0; i < n; i++) {
         scanf("%d", v+i);
-        v[i] = n-v[i];
-        p[i] = i;
-        gen.insert(pii(v[i], i));
-        s[i].insert(pii(v[i], i));
+        its[v[i]].push_back(i);
     }
 
     es = 2;
@@ -69,45 +79,49 @@ int main () {
 
         nx[es] = hd[a]; to[es] = b; hd[a] = es++;
         nx[es] = hd[b]; to[es] = a; hd[b] = es++;
-        deg[a]++; deg[b]++;
     }
 
-    for (int i = 0; i < n; i++)
-        if (deg[i] == 1)
-            qu[qf++] = i;
+    os = 0;
+    to[1] = 0;
+    dfs(1);
 
-    while (qi < qf) {
-        int u = qu[qi++];
-        pr[u] = 0;
-        deg[u] = 0;
+    ll qtd = 0;
 
-        for (int ed = hd[u]; ed; ed = nx[ed]) {
-            if (deg[to[ed]])
-                pr[u] = ed;
+    for (int _i = n; _i > 0; _i--) {
+        for (int _j = 0; _j < its[_i].size(); _j++) {
+            int i = its[_i][_j];
+
+            ll sum = 0;
+            //DEBUG("item %d\n", i);
+            for (int ed = hd[i]; ed; ed = nx[ed]) {
+                if (ed != pr[i]) {
+                    val[ed] = get(ord[to[ed]][1]-1) - get(ord[to[ed]][0]-1);
+                    sum += val[ed];
+                    //DEBUG("%d->%d : %lld\n", to[ed^1], to[ed], val[ed]);
+                }
+            }
+            if (pr[i]) {
+                val[pr[i]] = qtd - sum;
+                //DEBUG("%d->%d : %lld = %lld - %lld [%d]\n", to[pr[i]^1], to[pr[i]], val[pr[i]], qtd, sum, pr[i]);
+            } else {
+                //DEBUG("is root\n");
+            }
         }
-
-        if (pr[u]) {
-            int w = to[pr[u]];
-            val[pr[u]] = gen.order_of_key(pii(v[u], -1)) - s[p[u]].order_of_key(pii(v[u], -1));
-            //DEBUG("%d->%d : %lld = %d - %d\n", u, w, val[pr[u]], (int) gen.order_of_key(pii(v[u], -1)), (int) s[p[u]].order_of_key(pii(v[u], -1)));
-            val[pr[u]^1] = s[p[u]].order_of_key(pii(v[w], -1));
-            //DEBUG("%d->%d : %lld\n", w, u, val[pr[u]^1]);
-
-            join(u, w);
-
-            deg[w]--;
-            if (deg[w] == 1)
-                qu[qf++] = w;
-        } else {
-            assert(qi == qf);
+        for (int _j = 0; _j < its[_i].size(); _j++) {
+            add(ord[its[_i][_j]][0], 1);
+            qtd++;
         }
     }
+
+    for (int i = 2; i < es; i++)
+        pd(i);
 
     int res = 0;
     ll vres = LLONG_MAX;
     for (int i = 0; i < n; i++) {
-        to[0] = i;
-        ll att = pd(0);
+        ll att = 0;
+        for (int ed = hd[i]; ed; ed = nx[ed])
+            att += memo[ed];
         //DEBUG("%d : %lld\n", i, att);
         if (att < vres) {
             vres = att;
@@ -117,3 +131,4 @@ int main () {
 
     printf("%d %lld\n", res+1, vres);
 }
+
