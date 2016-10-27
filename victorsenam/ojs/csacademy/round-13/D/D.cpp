f4e5cf9e21f91e1ec95f4fff52fad2097f511ffc
Victor Sena Molero <victorsenam@gmail.com>
2016-10-27 18:13:29 -0200

    vaicurinthia

--- victorsenam/ojs/csacademy/round-13/D/D.cpp
+++ victorsenam/ojs/csacademy/round-13/D/D.cpp
@@ -15,69 +15,35 @@ const int M = 1e6+7;
 int n, m;
 int ts;
 
-vector<ll> pr;
-bool sieve[M];
-
-num get (ll x, int k) {
-    num ret = 1;
-    for (ll p : pr) {
-        if (p > x) break;
-        int cnt = 0;
-
-        while (x % p == 0) {
-            x /= p;
-            cnt += k;
-        }
-
-        cnt = (cnt%3+3)%3;
-        ll loc = 1;
-        for (int i = 0; i < cnt; i++)
-            loc *= p;
-        ret *= loc;
-
-    }
-    return ret;
-}
-
-int mp[3][M], turn;
+ll sieve[M];
+ll mp[3][M], turn;
 ll sv[2][M];
 
 int main () {
-    for (int k = 0; k < 2; k++)
-        for (int i = 0; i < M; i++)
-            sv[k][i] = 1;
-
+    sv[0][1] = sv[1][1] = 1;
     for (ll p = 2; p < M; p++) {
         if (!sieve[p]) {
-            int cnt = 1;
-            ll a = p;
-            ll b = p*p;
-            debug("multiplos de: \n");
-            for (ll curr = p; curr < M; curr *= p) {
-                debug("%lld: ", curr);
-                for (ll j = curr; j < M; j += curr) {
-                    if ((j/curr)%p == 0) continue;
-                    sieve[j] = 1;
-
-                    sv[0][j] *= a;
-                    sv[1][j] *= b;
-                }
-                debug("\n");
-
-                cnt = (cnt+1)%3;
-                if (cnt == 0) {
-                    a = b = 1;
-                } else if (cnt == 1) {
-                    a = p;
-                    b = p*p;
-                } else {
-                    a = p*p;
-                    b = p;
-                }
+            for (ll j = p; j < M; j += p) {
+                sieve[j] = p;
             }
         }
-        printf("%lld : %lld %lld\n", p, sv[0][p], get(p, 1));
-        if (sv[0][p] != get(p, 1)) break;
+
+        ll x = p;
+        int cnt = 0;
+        while (x % sieve[p] == 0) {
+            cnt++;
+            x /= sieve[p];
+        }
+
+        cnt %= 3;
+
+        ll a = sieve[p];
+        ll b = sieve[p]*sieve[p];
+        if (cnt == 2) swap(a, b);
+        else if (cnt == 0) a = b = 1;
+
+        sv[0][p] = sv[0][x]*a;
+        sv[1][p] = sv[1][x]*b;
     }
 
     scanf("%d", &ts);
@@ -101,10 +67,11 @@ int main () {
         for (int i = 0; i < m; i++) {
             scanf("%lld", &a);
             ll x = sv[1][a];
+            if (x >= M) continue;
             if (mp[2][x] != turn) continue;
             mp[1][x]++;
 
-            res = max(res, ll(mp[0][i])*ll(mp[0][i]) + ll(mp[1][i])*ll(mp[1][i]));
+            res = max(res, ll(mp[0][x])*ll(mp[0][x]) + ll(mp[1][x])*ll(mp[1][x]));
         }
 
         
