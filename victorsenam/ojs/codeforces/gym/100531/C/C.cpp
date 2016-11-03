f4c2c5602d5eaa88b2a38d006d67a7b3b0bef9ea
Victor Sena Molero <victorsenam@gmail.com>
2016-11-03 15:58:51 -0200

    antônio conselheiro

--- victorsenam/ojs/codeforces/gym/100531/C/C.cpp
+++ victorsenam/ojs/codeforces/gym/100531/C/C.cpp
@@ -14,36 +14,59 @@ stack<int> st;
 int lim[N][2];
 deque<int> adj[N];
 int es;
+int res;
 
-int solve (int u) {
-    int res = 0;
-    while (adj[u].size() != 0) {
-        debug("[%d,%d]\n", lim[u][0], lim[u][1]);
-        int ch = adj[u].front();
-        res += solve(ch);
-        adj[u].pop_front();
-        while (!adj[ch].empty()) {
-            int v = adj[ch].back();
-            adj[ch].pop_back();
-            adj[u].push_front(v);
+void printdebug (int u) {
+    debug("%02d ", u);
+    for (int i = 0; i < n; i++)
+        if (i < lim[u][0] || i > lim[u][1]) {
+            debug("-");
+        } else {
+            debug("%c", str[i]);
         }
+}
 
-        char vv = str[lim[adj[u].front()][0]];
-        debug("[%d,%d]: %c\n", lim[u][0], lim[u][1], vv);
-        if (vv == 'B') {
-            if (adj[u].size() >= 4) {
+void printalista (int u) {
+    for (int i : adj[u])
+        debug("%d ", i);
+}
+
+char solve (int u) {
+    if (lim[u][0] == lim[u][1]) {
+        printdebug(u);
+        debug("\n");
+        return str[lim[u][0]];
+    }
+    bool ok = 1;
+    while (adj[u].size() > 1 && ok) {
+        int ch = adj[u].front();
+        adj[u].pop_front();
+        char vv = solve(ch);
+
+        printdebug(u);
+        debug(" ");
+        printalista(u);
+
+        if (vv == 'X') {
+            ok = 0;
+        } else if (vv == 'B') {
+            if (adj[u].size() >= 3) {
                 adj[es].push_front(adj[u].back());
                 adj[u].pop_back();
 
                 adj[es].push_front(adj[u].back());
                 adj[u].pop_back();
+
+                adj[u].push_back(es);
 
                 es++;
                 res++;
-                adj[u].pop_front();
+                debug("x ");
+            } else {
+                ok = 0;
             }
         } else if (vv == 'C') {
-            if (adj[u].size() >= 4) {
+            if (adj[u].size() >= 3) {
                 int a = adj[u].back();
                 adj[u].pop_back();
                 int b = adj[u].back();
@@ -52,23 +75,44 @@ int solve (int u) {
                 adj[u].push_back(b);
 
                 res++;
-                adj[u].pop_front();
+                debug("x ");
+            } else {
+                ok = 0;
             }
         } else if (vv == 'K') {
-            if (adj[u].size() >= 3) {
+            if (adj[u].size() >= 2) {
                 adj[u].pop_back();
 
                 res++;
-                adj[u].pop_front();
+                debug("x ");
+            } else {
+                ok = 0;
             }
         } else {
-            if (adj[u].size() >= 2) {
+            if (adj[u].size() >= 1) {
                 res++;
-                adj[u].pop_front();
+                debug("x ");
+            } else {
+                ok = 0;
             }
         }
+
+        debug("-> ");
+        printalista(u);
+        debug("\n");
     }
-    return res;
+
+    char rr = 'X';
+    for (int i : adj[u]) {
+        rr = solve(i);
+    }
+
+    if (!ok)
+        rr = 'X';
+
+    printdebug(u);
+    debug(": %c\n", rr);
+    return rr;
 }
 
 int main () {
@@ -76,9 +120,9 @@ int main () {
     scanf(" %s", str);
     n = strlen(str);
 
-    st.push(n);
-    lim[es][1] = n;
-    es++;
+    st.push(0);
+    lim[0][1] = n;
+    es = 1;
     for (i = n-1; i >= 0; i--) {
         if (str[i] == ')') {
             lim[es][1] = i;
@@ -89,17 +133,26 @@ int main () {
             lim[v][0] = i;
 
             int u = st.top();
-            adj[u].push_back(v);
+            adj[u].push_front(v);
         } else {
             lim[es][0] = i;
             lim[es][1] = i;
             int u = st.top();
-            adj[u].push_back(es);
-            es++;           
+            adj[u].push_front(es++);
         }
     }
 
     lim[0][0] = -1;
 
-    printf("%d\n", solve(0));
+    for (int i = 0; i < es; i++) {
+        printdebug(i);
+        for (int j : adj[i])
+            debug(" %d", j);
+        debug("\n");
+    }
+
+    debug("\n");
+
+    solve(0);
+    printf("%d\n", res);
 }
