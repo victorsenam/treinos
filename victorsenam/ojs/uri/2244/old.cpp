36f598bde3280f327132df466bff7cc123eba744
Victor Sena Molero <victorsenam@gmail.com>
2016-11-11 19:47:20 -0200

    Fail

--- victorsenam/ojs/uri/2244/old.cpp
+++ victorsenam/ojs/uri/2244/old.cpp
@@ -15,7 +15,6 @@ const int N = 1e5+7;
 struct cvx {
     num sa[N], sb[N], st[N], id[N];
     int ss;
-    int lsi;
 
     inline num numdiv (num a, num b)
     { return a/b; }
@@ -57,7 +56,6 @@ struct cvx {
 
     num get (num x) { // da pra deixar linear se os x forem ordenados
         int l = upper_bound(st, st+ss, x) - st - 1; // MIN: comparar decrescente
-        lsi = id[l];
         DEBUG("get %d (%lld,%lld) for %lld\n", l, sa[l], sb[l], x);
         return sa[l]*x + sb[l];
     }
@@ -69,24 +67,17 @@ num inp[N][4];
 int p[N];
 int cmp_k;
 set<int> s;
-double pr;
 
 bool cmp_t (int i, int j) {
-    for (int k = 0; k < 4; k++) {
-        int nk = (k+cmp_k)%4;
-        if (inp[i][nk] != inp[j][nk])
-            return inp[i][nk] < inp[j][nk];
-    }
+    if (inp[i][cmp_k] != inp[j][cmp_k])
+        return inp[i][cmp_k] < inp[j][cmp_k];
+    if (inp[i][cmp_k+1] != inp[j][cmp_k+1])
+        return inp[i][cmp_k+1] < inp[j][cmp_k+1];
+    if (inp[i][(cmp_k^2)] != inp[j][(cmp_k^2)])
+        return inp[i][(cmp_k^2)] < inp[j][(cmp_k^2)];
+    if (inp[i][(cmp_k^2)+1] != inp[j][(cmp_k^2)+1])
+        return inp[i][(cmp_k^2)+1] < inp[j][(cmp_k^2)+1];
     return 0;
-}
-
-bool iseq (int i, int j) {
-    for (int k = 0; k < 2; k++) {
-        int nk = (k+cmp_k)%4;
-        if (inp[i][nk] != inp[j][nk])
-            return 0;
-    }
-    return 1;
 }
 
 void go (int i, num x) {
@@ -97,7 +88,6 @@ void go (int i, num x) {
 }
 
 int main () {
-    srand(time(NULL)); rand(); rand();
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++) {
@@ -108,32 +98,44 @@ int main () {
         inp[i][3] = -inp[i][3];
     }
 
-    for (cmp_k = 0; cmp_k < 4; cmp_k += 2) {
-        sort(p, p+n, cmp_t);
+    cmp_k = 0;
+    sort(p, p+n, cmp_t);
 
-        for (int _i = 0; _i < n; _i++) {
-            int i = p[_i];
+    for (int _i = 0; _i < n; _i++) {
+        int i = p[_i];
 
-            v[cmp_k/2].insert(inp[i][cmp_k], inp[i][cmp_k+1], i);              
+        v[0].insert(inp[i][0], inp[i][1], i);              
 
-            if (_i) {
-                if (iseq(i, p[_i-1])) {
-                    s.insert(i);
-                    s.insert(p[_i-1]);
-                }
+        if (_i) {
+            if (cmp_t(i, p[_i-1]) == cmp_t(p[_i-1], i)) {
+                s.insert(i);
+                s.insert(p[_i-1]);
             }
         }
     }
 
-    int hc = s.size();
-    for (int j = 0; j < 2; j++) {
-        bool ok = 1;
-        for (int i = 1; i < v[j].ss; i++) {
-            if (v[j].st[i] <= 0.) continue;
-            pr = double(rand())/double(INT_MAX);
-            go(v[j].id[i-1], pr*(v[j].st[i] + max(0., v[j].st[i-1])));
+    cmp_k = 2;
+    sort(p, p+n, cmp_t);
+
+    for (int _i = 0; _i < n; _i++) {
+        int i = p[_i];
+
+        v[1].insert(inp[i][2], inp[i][3], i);
+
+        if (_i) {
+            if (cmp_t(i, p[_i-1]) == cmp_t(p[_i-1], i)) {
+                s.insert(i);
+                s.insert(p[_i-1]);
+            }
         }
-        go(v[j].id[v[j].ss-1], max(v[j].st[v[j].ss-1], 0.) + 1.);
+    }
+    int hc = s.size();
+
+    for (int j = 0; j < 2; j++) {
+        for (int i = 1; i < v[j].ss; i++) {
+            go(v[j].id[i-1], .5*(v[j].st[i] + v[j].st[i-1]));
+        }
+        go(v[j].id[v[j].ss-1], v[j].st[v[j].ss-1] + 1.);
     }
 
     printf("%d\n", int(s.size()) - hc);
