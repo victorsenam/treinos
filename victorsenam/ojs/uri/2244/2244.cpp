53058b501ed62f83c27a6a888b0ba6005b045dcc
Victor Sena Molero <victorsenam@gmail.com>
2016-11-24 22:01:18 -0200

    Thatsimpossibru

--- victorsenam/ojs/uri/2244/2244.cpp
+++ victorsenam/ojs/uri/2244/2244.cpp
@@ -1,104 +1,84 @@
+// Convex Trick para Otimizar PDs onde as escolhas de estado são retas com coeficiente angular crescente (basta inverter o ângulo caso seja decrescente) e busca-se maximizar o valor alcançado (fácil de adaptar para mínimo)
+// Caso se inverta o ângulo, deve procurar o mínimo ao invés do máximo e vice-versa
+// Inserção O(n) amortizado
+// Busca O(log(n))
+// Alterações pra gerar o caso mínimo estão marcadas com MIN
+// http://wcipeg.com/wiki/Convex_hull_trick
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef unsigned long long int ull;
-typedef long long int ll;
 
-#define debug(...) {fprintf(stderr, __VA_ARGS__);}
+#ifndef ONLINE_JUDGE
+#define DEBUG(...) {} {fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__); }
+#else
+#define DEBUG(...) {}
+#endif
 
-const int N = 1e5+6;
+const int N = 1e5+7;
 
-struct num {
-    ll a, b;
-
-    num (ll x, ll y) {
-        if (y < 0) {
-            x = -x;
-            y = -y;
-        }
-        a = x;
-        b = y;
-    }
-    num () {}
-
-    bool operator < (const num & ot) const {
-        if (ot.b == 0 && b == 0)
-            return a < ot.a;
-        return a*ot.b < ot.a*b;
-    }
-    operator double () const
-    { return double(a)/double(b); }
-};
-
-struct line {
-    ll a, b;
-    int idx;
-
-    line (ll x, ll y, int i) : a(x), b(y), idx(i) {}
-    line () {}
-
-    bool operator == (const line & ot) const
-    { return (a == ot.a) && (b == ot.b); }
-
-    ll get (ll x) 
-    { return a*x + b; }
-
-    num inter (line & x) {
-        if (a == x.a) {
-            if (b <= x.b)
-                return num(-1, 0);
-            else
-                return num(1, 0);
-        }
-        return num(x.b-b, a-x.a);
-    }
-
-    ll comp (line & x, line & y)
-    { return inter(x) < inter(y); }
-
-    bool ang_comp (const num & ot) const
-    { return a < ot.a; }
-};
+typedef double num;
+typedef long long ll;
 
 struct cvx {
-    line st[N];
+    num sa[N], sb[N], st[N];
     int ss;
 
-    num getst (int i) {
-        if (i == 0) return num(-1, 0);
-        if (i == ss) return num(1, 0);
-        return st[i].inter(st[i-1]);
+    inline num numdiv (num a, num b)
+    { return (a+b-1)/b; } // MIN: a/b
+
+    inline num inter (int i) {
+        if (!i)
+            return -1./0.; // MIN: LLONG_MAX
+
+        return numdiv(sb[i]-sb[i-1], sa[i-1]-sa[i]);
     }
 
-    int add (line & ot) {
-        st[ss] = ot;
-        while (ss > 1 && !(getst(ss-1) < getst(ss)))
-            swap(st[ss], st[--ss]);
-        st[ss++] = ot;        
-        return ss-1;
+    int insert (num a, num b) {
+        if (ss && sa[ss-1] == a) {
+            if (sb[ss-1] >= b) // MIN: <=
+                return -1;
+
+            ss--;
+        }
+
+        sa[ss] = a;
+        sb[ss] = b;
+        st[ss] = inter(ss);
+
+        while (ss && st[ss] <= st[ss-1]) { // MIN: >=
+            sa[ss-1] = sa[ss];
+            sb[ss-1] = sb[ss];
+            ss--;
+            st[ss] = inter(ss);
+        }
+
+        DEBUG("insert (%f,%f) at %f\n", a, b, st[ss]);
+        return ss++;
     }
 
-    void print () {
-        debug("%d", st[0].idx);
-        for (int i = 1; i < ss; i++)
-            debug(" [%f] %d", double(getst(i)), i);
-        debug("\n");
+    inline void reset()
+    { ss = 0; }
+
+    num get (num x) { // da pra deixar linear se os x forem ordenados
+        int l = upper_bound(st, st+ss, x) - st - 1; // MIN: comparar decrescente
+        DEBUG("get %d (%f,%f) for %f\n", l, sa[l], sb[l], x);
+        return sa[l]*x + sb[l];
     }
 };
 
 int n;
-line v[2][N];
-int ps[2][N];
+ll v[N][2][2];
+int r[N][2];
 int p[N];
-int cmp_a;
+int k;
 cvx trk[2];
 
 bool cmp_t (int i, int j) {
-    if (v[cmp_a][i].a != v[cmp_a][j].a)
-        return v[cmp_a][i].a > v[cmp_a][j].a;
-    if (v[cmp_a][i].b != v[cmp_a][j].b)
-        return v[cmp_a][i].b < v[cmp_a][j].b;
-    return v[cmp_a][i].idx < v[cmp_a][j].idx;
+    if (v[i][k][0] == v[j][k][0])
+        return v[i][k][0] > v[j][k][0];
+    if (v[i][k][1] == v[j][k][1])
+        return v[i][k][1] > v[j][k][1];
+    return i < j;
 }
 
 int main () {
@@ -107,53 +87,53 @@ int main () {
     for (int i = 0; i < n; i++) {
         p[i] = i;
         for (int k = 0; k < 2; k++) {
-            v[k][i].idx = i;
-            scanf("%lld %lld", &v[k][i].b, &v[k][i].a);
-            if (k) {
-                v[k][i].b = -v[k][i].b;
-                v[k][i].a = -v[k][i].a;
+            for (int j = 1; j >= 0; j--) {
+                scanf("%lld", &v[i][k][j]);
+                if (k)
+                    v[i][k][j] = -v[i][k][j];
             }
         }
     }
 
-    for (int k = 0; k < 2; k++) {
-        cmp_a = k;
-        sort(p, p+n, cmp_t);
+    for (k = 0; k < 2; k++) {
+        DEBUG("== %d =========\n", k);
+        sort(p, p+n);
 
-        for (int i = 0; i < n; i++) {
-            debug("== %lld %lld\n", v[k][p[i]].a, v[k][p[i]].b);
-            ps[k][p[i]] = trk[k].add(v[k][p[i]]);
-            if (i && v[k][p[i]] == v[k][p[i-1]])
-                ps[k][p[i]] = -1;
+        for (int _i = 0; _i < n; _i++) {
+            int i = p[_i];
+
+            r[i][k] = trk[k].insert(v[i][k][0], v[i][k][1]);
+            if (v[i][k][0] == v[p[_i-1]][k][0] && v[i][k][1] == v[p[_i-1]][k][1])
+                r[i][k] = -1;
         }
-        trk[k].print();
+        trk[k].st[trk[k].ss] = 1./0.;
     }
 
     int res = 0;
     for (int i = 0; i < n; i++) {
         bool ok = 1;
-        num mi = num(0,1);
-        num ma = num(1,0);
+        double mi = 0.;
+        double ma = 1./0.;
 
+        DEBUG("-- %d --\n", i);
+    
         for (int k = 0; k < 2; k++) {
-            debug("%d ", ps[k][i]);
-            if (ps[k][i] == -1) ok = 0;
-            else if (trk[k].st[ps[k][i]].idx != i) ok = 0;
+            DEBUG("%d\n", r[i][k]);
+            if (r[i][k] == -1) ok = 0;
+            else if (trk[k].ss <= r[i][k]) ok = 0;
+            else if (trk[k].sa[r[i][k]] == v[i][k][0] && trk[k].sb[r[i][k]] == v[i][k][1]) ok = 0;
             else {
-                num a = trk[k].getst(ps[k][i]);
-                num b = trk[k].getst(ps[k][i]+1);
-                debug("(%f %f) ", double(a), double(b));
-
-                if (mi < a) mi = a;
-                if (b < ma) ma = b;
+                mi = max(mi, trk[k].st[r[i][k]]);
+                ma = min(ma, trk[k].st[r[i][k]+1]);       
             }
         }
 
+        DEBUG("[%d] [%f %f]\n", ok, mi, ma);
+
         if (ok && mi < ma) {
-            debug("[%d] : (%f %f)", i, double(mi), double(ma));
             res++;
         }
-        debug("\n");
     }
+
     printf("%d\n", res);
 }
