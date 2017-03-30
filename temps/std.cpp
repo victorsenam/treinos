4282b4adbd3efb6ce92f109557cf956f9a50c6c3
Victor Sena Molero <victorsenam@gmail.com>
2017-03-30 20:24:43 -0300

    colocando debug condicional

--- temps/std.cpp
+++ temps/std.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
+#ifndef ONLINE_JUDGE
 #define debug(...) {fprintf(stdout, __VA_ARGS__);}
+#else
+#define debug(...) {}
+#endif
 
 using namespace std;
 typedef long long int ll;
