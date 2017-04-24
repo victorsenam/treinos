b501dee035e3c523d3428f4b4cf42e8517d177be
Victor Sena Molero <victorsenam@gmail.com>
2017-04-24 18:59:17 -0300

    Na tentativa vou ficar porque eu preciso entender

--- ojs/codeforces/793/C/C.cpp
+++ ojs/codeforces/793/C/C.cpp
@@ -7,103 +7,75 @@
 
 using namespace std;
 typedef long long int ll;
-typedef long double ld;
 typedef pair<int, int> pii;
 #define pb push_back
 
 const int N = 1e5+7;
-const ld eps = 1e-8;
-const ld INF = N;
 
-ll b[4];
-ll p[N][2], v[N][2];
 int n;
+ll w[2][2];
+ll p[N][2][2];
 
 struct quo {
     ll p, q;
 
     quo (ll a, ll b) {
-        if (b == 0) a = 1;
+        if (a == 0)
+            b = 1;
 
         if (b < 0) {
-            a = -a;
             b = -b;
+            a = -a;
         }
-        
-        ll g = __gcd(abs(a), b);
 
+        ll g = __gcd(abs(a),b);
         p = a/g;
         q = b/g;
     }
 
-    bool operator < (const quo & ot) const {
-        if (q == 0) return 0;
-        if (ot.q == 0) return 1;
-        return p*ot.q < q*ot.p;
+    bool operator < (const quo & x) const {
+        if (!q && !x.q)
+            return p < x.p;
+        return p*x.q < q*x.p;
     }
 };
 
-void go (quo & lo, quo x) {
-    if (x.q == 0) {
-        printf("-1\n");
-        exit(0);
-    }
-    if (lo < x)
-        lo = x;
-}
-
-void tes (quo lo, quo x) {
-    if (lo.q == 0) return;
-    if (lo < x)
-        return;
-    printf("-1\n");
-    exit(0);
-}
-
-quo get (ll p, ll v, ll x) {
-    if (p >= x) return quo(0,1);
-    if (v <= 0) return quo(1,0);
-
-    return quo(x-p, v);
-}
-
 int main () {
     scanf("%d", &n);
-    for (int i = 0; i < 4; i++)
-        scanf("%lld", &b[i]);
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < 2; i++)
         for (int j = 0; j < 2; j++)
-            scanf("%lld", &p[i][j]);
-        for (int j = 0; j < 2; j++)
-            scanf("%lld", &v[i][j]);
-    }
+            scanf("%lld", &w[i][j]);
+
 
     quo lo(0,1);
+    quo hi(1,0);
 
-    for (int i = 0; i < n; i++) {
-        quo a = max(get(p[i][0], v[i][0], b[0]), get(-p[i][0], -v[i][0], -b[2]));
-        lo = max(a, lo);
-        a = max(get(p[i][1], v[i][1], b[1]), get(-p[i][1], -v[i][1], -b[3]));
-        lo = max(a, lo);
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                scanf("%lld", &p[k][i][j]);
+        
+        for (int j = 0; j < 2; j++) {
+            quo a(w[0][j] - p[k][0][j], p[k][1][j]);
+            quo b(w[1][j] - p[k][0][j], p[k][1][j]);
 
+            if (p[k][1][j] == 0) {
+                if (p[k][0][j] <= w[0][j] || p[k][0][j] >= w[1][j])
+                    lo = quo(1,0);
+            }
+
+            if (b < a)
+                swap(a, b);
+
+            lo = max(a, lo);
+            hi = min(b, hi);
+        }
     }
-    if (lo.q == 0) {
+
+    if (lo < hi)
+        printf("%.20f\n", double(lo.p)/double(lo.q));
+    else
         printf("-1\n");
-        return 0;
-    }
-
-    for (int i = 0; i < n; i++) {
-        quo a = max(get(-p[i][0], -v[i][0], -b[0]), get(p[i][0], v[i][0], b[2]));
-        if (!(lo < a)) {
-            printf("-1\n");
-            return 0;
-        }
-        a = max(get(-p[i][1], -v[i][1], -b[1]), get(p[i][1], v[i][1], b[3]));
-        if (!(lo < a)) {
-            printf("-1\n");
-            return 0;
-        }
-    }
-
-    printf("%.20Lf\n", ld(lo.p)/ld(lo.q));
 }
+
