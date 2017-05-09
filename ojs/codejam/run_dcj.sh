48fff39fb1c832e647aa5a715efc0de268e367b3
Victor Sena Molero <victorsenam@gmail.com>
2017-05-08 22:39:37 -0300

    DCJ

---
+++ ojs/codejam/run_dcj.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIRECTORY=$( cd "$( dirname $0 )" && pwd )
+PYTHON="/usr/bin/python2.7"
+
+NODES=${2:='100'}
+
+$PYTHON $DIRECTORY/dcj/dcj.py test --source=$1 --nodes=$NODES --output=all
