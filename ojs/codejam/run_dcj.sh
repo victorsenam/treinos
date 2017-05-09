44d5ea57b8de573eacb98a742b1bb60c1b059a3d
Victor Sena Molero <victorsenam@gmail.com>
2017-05-08 22:44:22 -0300

    Fixing run_dcj.sh

--- ojs/codejam/run_dcj.sh
+++ ojs/codejam/run_dcj.sh
@@ -3,6 +3,6 @@
 DIRECTORY=$( cd "$( dirname $0 )" && pwd )
 PYTHON="/usr/bin/python2.7"
 
-NODES=${2:='100'}
+NODES=${2:-'100'}
 
 $PYTHON $DIRECTORY/dcj/dcj.py test --source=$1 --nodes=$NODES --output=all
