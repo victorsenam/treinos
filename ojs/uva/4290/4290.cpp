46940e32831c708e06e1ff62d631ff70e7030dd8
Victor Sena Molero <victorsenam@gmail.com>
2017-06-01 00:52:41 -0300

    robô

--- ojs/uva/4290/4290.cpp
+++ ojs/uva/4290/4290.cpp
@@ -11,70 +11,70 @@ typedef long long int ll;
 typedef pair<ll,ll> pii;
 #define pb push_back
 
-struct itv {
-    ll l,r;
-    ll a,b;
-
-    bool operator < (itv o) {
-        if (l != o.l) return l < o.l;
-        if (r != o.r) return r < o.r;
-        if (a != o.a) return a < o.a;
-        return b < o.b;
-    }
-
-    itv expand_cons () {
-        itv nw = *this;
-        nw.b = 0;
-        if (b >= 0) {
-            nw.l = l - d;
-            nw.r = l + d - 1;
-        } else {
-            nw.l = r - d + 1;
-            nw.r = r + d;
-        }
-        return nw;
-    }
-
-    itv expand_chng () {
-        itv nw = *this;
-        if (b >= 0) {
-            nw.l = l + d;
-            nw.r = r + d;
-        } else {
-            nw.l = l - d;
-            nw.r = r - d;
-        }
-        return nw;
-    }
-};
-
-const int N = 1e3;
+const int N = 2e3;
+const ll INF = 1e16;
 
 int ts;
 ll n, d;
-ll v[N];
-set<itv> s[2];
-itv aux;
+ll h[N];
+ll p[N*N], ps;
+ll dp[2][2*N*N + N];
 
 int main () {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin >> ts;
+    while (ts--) {
+        cin >> n >> d;
 
-    cin >> n >> d;
-    for (int i = 0; i < n; i++)
-        cin >> v[i];
+        for (int i = 0; i < n; i++) {
+            cin >> h[i];
+            p[i] = h[i];
+        }
+        ps = n;
 
-    aux.a = 0;
-    aux.b = 0;
-    aux.l = aux.r = v[0];
+        sort(p, p+n);
 
-    s[0].clear();
-    s[0].insert(aux);
+        for (int i = 0; i < n; i++) {
+            for (int j = -n; j <= n; j++) {
+                int to = h[i] + j*d;
+                if (to < p[0] || to > p[n-1]) continue;
+                p[ps++] = to;
+            }
+        }
 
-    for (int i = 1; i < n; i++) {
-        s[i&1].clear();
+        sort(p, p+ps);
+        ps = unique(p, p+ps) - p;
 
-        for (itv x : s[0]) {
+        for (int i = 0; i < ps; i++) {
+            dp[0][i] = INF;
+            if (p[i] == h[0])
+                dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            multiset<ll> s;
+            int l = 0, r = 0;
+            for (int j = 0; j < ps; j++) {
+                while (r < ps && abs(p[j] - p[r]) <= d) {
+                    s.insert(dp[!(i&1)][r]);
+                    r++;
+                }
+                while (l < r && abs(p[j] - p[l]) > d) {
+                    s.erase(s.find(dp[!(i&1)][l]));
+                    l++;
+                }
+                dp[i&1][j] = (*s.begin());
+                dp[i&1][j] += abs(h[i] - p[j]);
+                dp[i&1][j] = min(dp[i&1][j], INF);
+            }
+        }
+
+        for (int i = 0; i < ps; i++) {
+            if (p[i] == h[n-1]) {
+                if (dp[!(n&1)][i] == INF)
+                    cout << "impossible" << endl;
+                else
+                    cout << dp[!(n&1)][i] << endl;
+            }
         }
     }
 }
