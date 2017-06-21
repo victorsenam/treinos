a8b429b20d09d43e800f774f974e57d605a1ef94
Victor Sena Molero <victorsenam@gmail.com>
2017-06-20 21:49:25 -0300

    Geometry is still hard

--- ojs/opentrains/1425/G/G.cpp
+++ ojs/opentrains/1425/G/G.cpp
@@ -11,10 +11,17 @@ typedef long long int ll;
 typedef pair<ll,ll> pii;
 #define pb push_back
 
-typedef double cood;
-const double eps = 1e-9;
+ll gcd (ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a,b);
+    }
+    return a;
+}
 
-const double pi = acos(-1.);
+typedef double cood;
+cood eps = 1e-9;
+double pi = acos(-1.);
 
 struct vec { // vector
 	// === BASIC ===
@@ -23,6 +30,8 @@ struct vec { // vector
 	vec (cood a, cood b) : x(a), y(b) {}
 	friend ostream& operator<<(ostream& os, vec o);
 
+    bool operator < (const vec & o) const
+    { return x + eps < o.x || (!(o.x + eps < x) && y + eps < o.y); }
 	vec operator - (vec o)
 	{ return vec(x - o.x, y - o.y); }
 	vec operator + (vec o)
@@ -39,48 +48,18 @@ struct vec { // vector
 	cood sq (vec o = vec())
 	{ return ((*this)-o)*((*this)-o); }
 	double nr (vec o = vec())
-	{ return sqrt(sq(o)); }
+	{ return sqrt(double(sq(o))); }
 
 	cood ar (vec a, vec b) // ccw signed area (positive if this is to the left of ab)
 	{ return (b - a) ^ ((*this) - a); }
 	int sd (vec a, vec b) // which side is this from ab? (-1 left, 0 over, 1 right)
-	{ cood o = ar(a, b); return (o < -eps) - (o > eps); }
+	{ cood o = ar(a, b); return (o < -eps) - (eps < o); }
 
 	// === ADVANCED ===
-	// rotate ccw by a (fails with ll)
-	vec rotate (double a)
-	{ return vec(cos(a) * x - sin(a) * y, sin(a) * x + cos(a) * y); }
-
-	// divide the plane relative to anc
-	// 0 if the ccw angle from anc to this is in [0,pi) and 1 otherwise, origin goes to 0
-	bool halfplane (vec anc = vec(1,0)) {
-		cood l = sd(vec(), anc);
-		if (l == 0)
-			return (x < -eps);
-		return (l == 1);
-	}
-
-	// ordering (ccw angle from anc, distance to origin)
-	// is this < o?
-	// PRECISION : ok with double if norm in [-1e4,1e3]
-	bool compare (vec o, vec anc = vec(1,0)) {
-		bool s[2] = {halfplane(anc), o.halfplane(anc)};
-		if (s[0] != s[1])
-			return s[0] < s[1];
-
-		cood l = sd(o, vec());
-		if (l == 0)
-			return sq() < o.sq() - eps;
-		return (l == -1);
-	}
-
-	// tests TODO
-	// is this inside segment st? (tip of segment included, change for -eps otherwise)
+	// is this inside segment st? (tip of segment included, change for < -eps otherwise)
 	bool in_seg (vec s, vec t)
-	{ return (sd(s, t) == 0) && ((*this) - s) * ((*this) - t) < eps; }
+	{ return (sd(s, t) == 0) && !(-eps < ((*this) - s) * ((*this) - t)); }
 };
-ostream& operator<<(ostream& os, vec o)
-{ return os << '(' << o.x << ", " << o.y << ')'; }
 
 // tests TODO
 struct lin { // line
@@ -96,8 +75,8 @@ struct lin { // line
 	// line intersection
 	vec inter (lin o) {
 		cood d = a * o.b - o.a * b;
-		if (abs(d) < eps) throw 0; // parallel
-		return vec((o.b * c - b * o.c) / d, (a * o.c - o.a * c) / d);
+		if (!(d < -eps) && !(eps < d)) throw 0; // parallel
+		return vec(cood((o.b * c - b * o.c) / d), cood((a * o.c - o.a * c) / d));
 	}
 };
 
@@ -108,139 +87,154 @@ bool seg_inter (vec a, vec b, vec c, vec d) {
 		return true;
 	return (c.sd(a, b) * d.sd(a, b) == -1 && a.sd(c, d) * b.sd(c, d) == -1);
 }
+ostream & operator << (ostream & o, vec x)
+{ return o << "(" << x.x << "," << x.y << ")"; }
 
-const int N = 307;
-const int M = 1e6+7;
+const int N = 1e6;
 
 int n, m;
-vec pp[N][2];
-vec v[M];
-int ty[M];
-int visi[M], turn;
+vec v[N];
+int ls[N][2];
+vector<int> inl[N];
 
-bool operator< (vec a, vec b) {
-    if (a.x < b.x - eps || a.x > b.x + eps) return a.x < b.x - eps;
-    return a.y < b.y - eps;
+int visi[N];
+bool istp[N];
+int snk;
+int src;
+vector<int> adj[N];
+
+bool mark (int u) {
+    if (visi[u])
+        return 1;
+    //cout << v[u] << endl;
+    if (u == src)
+        return 0;
+    visi[u] = 1;
+    if (istp[u])
+        return 1;
+
+    for (int v : adj[u])
+        if (!mark(v))
+            return 0;
+    return 1;
 }
 
-map<vec, int> mp;
-vector<int> pts[N];
-vector<pair<int, double> > adj[M];
-
-double dfs (int u, int p, int t, int d) {
-    if (u == 0 && t == 0) {
-        printf("-1\n");
-        exit(0);
-    }
-    assert(u != 1 || t == 0);
-    if (ty[u] == t) {
-        ty[u] = d;
+double dfs (int u, int v) {
+    if (visi[u])
         return 0;
-    }
-    if (visi[u] == turn || ty[u] == d)
-        return 0;
-    visi[u] = turn;
+    visi[u] = 1;
 
-    double rr = 0.;
-    for (pair<int, double> v : adj[u]) {
-        if (v.first == p) continue;
-        rr += v.second + dfs(v.first, u, t, d);
+    double res = 0.;
+    for (int w : adj[u]) {
+        if (w == v) continue;
+        res += ::v[u].nr(::v[w]);
+        res += dfs(w, u);
     }
-    return rr;
+
+    return res;
 }
 
 int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
+    double chk = 0;
+    map<vec, int> mp;
     cin >> n >> m;
+    int qs = 0;
+    for (int i = 0; i < n+n+m+2; i++) {
+        cin >> v[qs].x >> v[qs].y;
 
-    double res = 0.;
-    double check = 0.;
-
-    for (int i = 0; i < n; i++) {
-        cin >> pp[i][0].x >> pp[i][0].y >> pp[i][1].x >> pp[i][1].y;
-        res += pp[i][0].nr(pp[i][1]);
-    }
-    m += 2;
-    for (int i = 2; i < m; i++) {
-        cin >> v[i].x >> v[i].y;
-        mp[v[i]] = i;
-        ty[i] = 0; // closing
-
-        for (int j = 0; j < n; j++)
-            if (v[i].in_seg(pp[j][0], pp[j][1]))
-                pts[j].push_back(i);
-    }
-    for (int i = 0; i < 2; i++) {
-        cin >> v[i].x >> v[i].y;
-        mp[v[i]] = i;
-        ty[i] = 1; // spec
-
-        for (int j = 0; j < n; j++)
-            if (v[i].in_seg(pp[j][0], pp[j][1]))
-                pts[j].push_back(i);
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 2; j++) {
-            v[m] = pp[i][j];
-            if (mp.find(v[m]) != mp.end()) continue;
-            mp[v[m]] = m;
-            ty[m] = 4;
-            pts[i].push_back(m);
-            m++;
+        int cur = qs;
+        if (mp.find(v[qs]) == mp.end()) {
+            mp[v[qs]] = qs++;
+        } else {
+            cur = mp[v[qs]];
         }
-        for (int j = 0; j < i; j++) {
-            if (seg_inter(pp[i][0], pp[i][1], pp[j][0], pp[j][1])) {
-                ty[m] = 0;
-                for (int a = 0; a < 2 && !ty[m]; a++)
-                    for (int b = 0; b < 2 && !ty[m]; b++)
-                        if (abs(pp[i][a].x - pp[j][b].x) < eps && abs(pp[i][a].y - pp[j][b].y) < eps) {
-                            v[m] = pp[i][a];
-                            ty[m] = 2;
-                        }
-                if (ty[m] != 2) {
-                    ty[m] = 2; // inter
-                    v[m] = lin(pp[i][0], pp[i][1]).inter(lin(pp[j][0], pp[j][1]));
-                }
-                
-                int idx = m;
-                if (mp.find(v[m]) != mp.end())
-                    idx = mp[v[m]];
+
+        if (i < n+n) {
+            ls[i/2][i%2] = cur;
+            if (i%2 == 1)
+                chk += v[ls[i/2][0]].nr(v[ls[i/2][1]]);
+        } else if (i < n+n+m) {
+            istp[cur] = 1;
+        } else if (i == n+n+m) {
+            src = cur;
+        } else {
+            snk = cur;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < qs; j++) {
+            if (v[j].in_seg(v[ls[i][0]],v[ls[i][1]])) {
+                //cout << j << ": " << v[j] << " in " << i << ": " << v[ls[i][0]] << " " << v[ls[i][1]] << endl;
+                inl[i].push_back(j);
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        inl[i].push_back(ls[i][0]);
+        inl[i].push_back(ls[i][1]);
+        for (int j = i+1; j < n; j++) {
+            if (seg_inter(v[ls[i][0]], v[ls[i][1]], v[ls[j][0]], v[ls[j][1]])) {
+                int cur = qs;
+                if (v[ls[i][0]].in_seg(v[ls[j][0]], v[ls[j][1]]))
+                    cur = ls[i][0];
+                else if (v[ls[i][1]].in_seg(v[ls[j][0]], v[ls[j][1]]))
+                    cur = ls[i][1];
+                else if (v[ls[j][0]].in_seg(v[ls[i][0]], v[ls[i][1]]))
+                    cur = ls[j][0];
+                else if (v[ls[j][1]].in_seg(v[ls[i][0]], v[ls[i][1]]))
+                    cur = ls[j][1];
                 else {
-                    mp[v[m]] = m;
-                    m++;
+                    v[qs] = lin(v[ls[i][0]], v[ls[i][1]]).inter(lin(v[ls[j][0]], v[ls[j][1]]));
+
+                    if (mp.find(v[qs]) == mp.end()) {
+                        mp[v[qs]] = qs++;
+                    } else {
+                        cur = mp[v[qs]];
+                    }
                 }
 
-                pts[i].push_back(idx);
-                pts[j].push_back(idx);
+                inl[i].push_back(cur);
+                inl[j].push_back(cur);
             }
         }
     }
 
+    double res = 0;
     for (int i = 0; i < n; i++) {
-        sort(pts[i].begin(), pts[i].end(), [i] (int x, int y) {
-            return pp[i][0].sq(v[x]) < pp[i][0].sq(v[y]);
+        sort(inl[i].begin(), inl[i].end(), [i] (int a, int b) {
+            cood da = v[ls[i][0]].sq(v[a]); cood db = v[ls[i][0]].sq(v[b]);
+            if (da < db) return true;
+            if (db < da) return false;
+            assert(a == b);
+            return a < b;
         });
 
-        cout << "segm " << pp[i][0] << " " << pp[i][1] << endl;
-        cout << "[" << pts[i][0] << "] " << v[pts[i][0]] << " ty=" << ty[pts[i][0]] << endl;
-        for (int j = 1; j < pts[i].size(); j++) {
-            if (pts[i][j-1] == pts[i][j]) continue;
-            cout << "[" << pts[i][j] << "] " << v[pts[i][j]] << " ty=" << ty[pts[i][j]] << endl;
-            double dist = v[pts[i][j-1]].nr(v[pts[i][j]]);
-            adj[pts[i][j-1]].push_back(pair<int, double>(pts[i][j],dist));
-            adj[pts[i][j]].push_back(pair<int, double>(pts[i][j-1],dist));
-            check += dist;
+        inl[i].resize(unique(inl[i].begin(), inl[i].end()) - inl[i].begin());
+
+/*
+        for (int vv : inl[i])
+            cout << v[vv] << " ";
+        cout << endl;
+*/
+
+        for (int j = 1; j < inl[i].size(); j++) {
+            adj[inl[i][j-1]].push_back(inl[i][j]);
+            adj[inl[i][j]].push_back(inl[i][j-1]);
+            res += double(v[inl[i][j]].nr(v[inl[i][j-1]]));
         }
     }
 
-    assert(abs(check - res) < eps);
+    assert(abs(res-chk) < eps);
 
-    turn++;
-    dfs(1, 1, 0, 3);
-    turn++;
-    res -= dfs(0, 0, 3, -1);
+    if (!mark(snk)) {
+        cout << -1 << endl;
+        return 0;
+    }
+    res -= dfs(src, src);
     cout << setprecision(20) << res << endl;
 }
