902d3c8787312544d50660ff08be08c7875d724e
Victor Sena Molero <victorsenam@gmail.com>
2017-06-23 14:35:22 -0300

    Sla

---
+++ ojs/uri/2559/2559.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+//#define ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
+#define debug(...) {fprintf(stdout, __VA_ARGS__);}
+#else
+#define debug(...) {}
+#endif
+
+using namespace std;
+typedef long long int ll;
+typedef pair<ll,ll> pii;
+#define pb push_back
+
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
