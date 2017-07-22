dd964d2f13e764b4378816fffdbaf4ec90a336d1
Victor Sena Molero <victorsenam@gmail.com>
2017-07-22 00:52:39 -0300

    wohol!

--- ojs/vjudge/172520/J/J.cpp
+++ ojs/vjudge/172520/J/J.cpp
@@ -5,36 +5,42 @@ typedef long long ll;
 typedef pair<ll,ll> pii;
 #define pb push_back
 
-const int N = 1e6+7;
+const int N = 1e6+2;
+const int M = 2e5+2;
 
-struct possi {
-    ll s, f;
+struct posi {
+    int s;
+    int f;
     int conte;
-    ll prev_s;
-
-    bool operator < (possi ot) const {
-        return s < ot.s;
-    }
+    int prev_s;
+    int from;
 };
 
 int n;
-ll s, dt;
-ll d[N], sn[N], st[N];
-ll l, r;
-possi v[N];
-set<posi> rm[N];
+int s, dt;
+int d[M], sn[M], st[M];
+int l, r;
+int v[N];
+vector<posi> pp;
 
-void addpoint (possi p) {
-    if (p.f < 0) return;
-    while (r < p.s)
+void addpoint (int p) {
+    if (pp[p].s < l || pp[p].f < 0) return;
+    while (r < pp[p].s)
         v[++r] = p;
-    if (v[p.s].f >= p.f) {
-        v[p.s] = p;
-        rm[p.conte].erase(
+    if (v[pp[p].s] == -1 || pp[v[pp[p].s]].f > pp[p].f) {
+        v[pp[p].s] = p;
     }
 }
 
+void printa () {
+    cout << l << "[";
+    for (int i = l; i <= r; i++)
+        cout << " " << pp[v[i]].f;
+    cout << " ]" << r << endl;
+}
+
 int main () {
+    memset(v, -1, sizeof v);
     ios::sync_with_stdio(0);
     cin.tie(0);
 
@@ -43,18 +49,19 @@ int main () {
         cin >> d[i] >> sn[i] >> st[i];
 
     l = r = s;
-    addpoint(possi({s, 0, -1, -1}));
+    pp.pb(posi({s, 0, -1, -1, -1}));
+    addpoint(0);
+    //printa();
 
     int ls = 0;
-    int qq = 0;
+    int ladd = 1;
     for (int dd = 0; dd < N; dd++) {
         int k = ls;
         while (d[k] == dd)
             k++;
         
         vector<int> conts;
-        conts.reserve(k-ls+3);
-        for (int i = ls; i < k; i++)
+            for (int i = ls; i < k; i++)
             conts.pb(i);
 
         sort(conts.begin(), conts.end(), [] (int i, int j) {
@@ -64,39 +71,50 @@ int main () {
         int ss = 0;
         for (int _i = 0; _i < conts.size(); _i++) {
             int i = conts[_i];
-            if (ss && st[i] <= st[ss-1])
+            if (ss && st[i] <= st[conts[ss-1]])
                 continue;
             conts[ss++] = i;
         }
+        //conts.resize(ss);
 
-        vector<possi> pp;
         int _i = -1;
         for (int j = l; j <= r; j++) {
-            pp.pb(possi({j, v[j].f/2, v[j].conte, j})); // nao usar contest
+            if (j != r && pp[v[j]].f == pp[v[j+1]].f)
+                continue;
+            pp.pb(posi({j, pp[v[j]].f/2, pp[v[j]].conte, j, pp[v[j]].from})); // nao usar contest
 
             while (_i + 1 < ss && sn[conts[_i+1]] <= j)
                 _i++;
             if (_i == -1) continue;
             int i = conts[_i];
-            pp.pb(possi({st[i]-v[j].f, v[j].f+dt, i, j})); // usar contest
+            pp.pb(posi({st[i]-(pp[v[j]].f), (pp[v[j]].f)+dt, i, j, v[j]})); // usar contest
         }
 
-        for (possi p : pp)
-            addpoint(p);
+        while (ladd < pp.size())
+            addpoint(ladd++);
 
-        while (l < r && v[l+1].f == 0)
+        while (l < r && pp[v[l+1]].f == 0)
             l++;
 
+        for (int j = l; j <= r; j++)
+            if (j != r && pp[v[j]].f > pp[v[j+1]].f)
+                pp[v[j]].f = pp[v[j+1]].f;
+
+        //printa();
         ls = k;
     }
 
-    stack<posi> ll;
+    stack<int> ll;
     ll.push(v[r]);
-    while (ll.top().conte != -1) {
-        posi aux;
-        aux.s = ll.top().prev_s;
-        ll.push(*(rm[ll.top().conte].find(aux)));
+    while (pp[ll.top()].from != -1) {
+        ll.push(pp[ll.top()].from);
     }
 
-    cout << r << endl;
+    cout << r << " " << ll.size() - 1 << endl;
+    ll.pop();
+    while (ll.size()) {
+        cout << pp[ll.top()].conte + 1 << " ";
+        ll.pop();
+    }
+    cout << endl;
 }
