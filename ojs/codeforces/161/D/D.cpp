3fbd45ab3bed912d3046ec14e9311189936bf2a0
Victor Sena Molero <victorsenam@gmail.com>
2017-08-01 11:34:27 -0300

    Cade o Nathan?

--- ojs/codeforces/161/D/D.cpp
+++ ojs/codeforces/161/D/D.cpp
@@ -1,62 +1,92 @@
 #include <bits/stdc++.h>
+#define debug if (1)
 
 using namespace std;
+typedef long long int ll;
+typedef pair<ll,ll> pii;
+#define pb push_back
 
-#define N 50000
-#define K 500
+const int N = 1e5+7;
 
-unsigned int d[N][K+1];
-vector<unsigned int> g[N];
-bool s[N];
-int n, k, a, b, root, p;
+int n, k;
+vector<int> adj[N];
+ll cn[N];
+int sz[N];
 
-long long int  contaPares (int v) {
-    long long int r = 0, comb = 0;
-    if (s[v]) return 0;
-    d[v][0] = 1;
-    s[v] = 1;
-
-    for (vector<unsigned int>::iterator it = g[v].begin(); it != g[v].end(); it++) {
-        r += contaPares(*it);
-        for (int i = 0; !s[*it] && i < k && d[*it][i]; i++) d[v][i+1] += d[*it][i];
+int getsiz (int u, int p) {
+    sz[u] = 1;
+    for (int v : adj[u]) {
+        if (v == p || sz[v] == -1)
+            continue;
+        sz[u] += getsiz(v,u);
     }
+    return sz[u];
+}
 
-    for (vector<unsigned int>::iterator it = g[v].begin(); it != g[v].end(); it++) {
-        if (s[*it]) continue;
+ll dfs (int u, int p, int d) {
+    if (d > k) return 0;
+    ll rs = cn[k-d];
+    for (int v : adj[u]) {
+        if (v == p || sz[v] == -1) continue;
+        rs += dfs(v, u, d+1);
+    }
+    return rs;
+}
 
- /*
-        printf("%d : ", v+1);
-        for (int i = 0; i <= k; i++) printf("%d ", d[*it][i]);
-        printf("\n");
-*/
+void add (int u, int p, int d) {
+    if (d > k) return;
+    cn[d]++;
+    for (int v : adj[u]) {
+        if (v == p || sz[v] == -1) continue;
+        add(v, u, d+1);
+    }
+}
 
-        for (int i = 0; i <= k-2; i++) {
-  //          printf("%d : %d[%d]*(%d[%d]-%d[%d]) = %d\n", v+1, *it+1, i, v+1, k-1-i, *it+1, k-2-i, d[*it][i]*(d[v][k-1-i] - d[*it][k-2-i]));
-            comb += d[*it][i]*(d[v][k-1-i] - d[*it][k-2-i]);
+ll cent (int u) {
+    ll rs = 0;
+    int siz = getsiz(u, u);
+
+    int w = u;
+    do {
+        u = w;
+        for (int v : adj[u]) {
+            if (sz[v] == -1 || sz[v] >= sz[u] || sz[v] + sz[v] < siz)
+                continue;
+            w = v;
+            break;
         }
+    } while (w != u);
+
+    sz[u] = -1;
+    for (int i = 0; i <= k; i++)
+        cn[i] = 0;
+    cn[0]++;
+
+    for (int v : adj[u]) {
+        if (sz[v] == -1) continue;
+        rs += dfs(v, v, 1);
+        add(v, v, 1);
     }
 
-    r += d[v][k];
-    r += comb/2;
-    s[v] = 0;
-
-    return r;
+    for (int v : adj[u]) {
+        if (sz[v] == -1) continue;
+        rs += cent(v);
+    }
+    
+    return rs;
 }
 
 int main () {
-    scanf("%d %d", &n, &k);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    for (int i = 0; i < n; i++) {
-        s[i] = 0;
-        for (int j = 0; j <= k; j++) d[i][j] = 0;
-
-        if (i) {
-            scanf("%d %d", &a, &b);
-
-            g[--a].push_back(--b);
-            g[b].push_back(a);
-        }
+    cin >> n >> k;
+    for (int i = 1; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a-1].pb(b-1);
+        adj[b-1].pb(a-1);
     }
 
-    printf("%I64d\n", contaPares(root));
+    cout << cent(0) << endl;
 }
