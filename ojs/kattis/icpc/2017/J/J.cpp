5c724bf167887608a341deb679283a7f075344fc
Victor Sena Molero <victorsenam@gmail.com>
2017-08-09 14:42:22 -0300

    Fail

--- ojs/kattis/icpc/2017/J/J.cpp
+++ ojs/kattis/icpc/2017/J/J.cpp
@@ -118,11 +118,15 @@ struct dinic {
 int n, m;
 double v, a;
 
-int ar[N][2];
+int ar[M][2];
 double wg[M];
 dinic flw;
 double m_flu, m_wat, m_mix;
 double res[M][2];
+double amn[M];
+
+int deg[N];
+double inp[N][2];
 
 double calc (double flu) {
     double wat = m_mix - flu;
@@ -182,34 +186,63 @@ int main () {
 
     debug cout << lo << " and " << m_mix - lo << endl;
 
+    cout << setprecision(20) << fixed;
+
     memset(flw.fl, 0, sizeof flw.fl);
     flw.cp[flu] = lo;
-    flw.cp[wat] = 0;
-    assert(abs(flw.maxflow() - lo) < eps);
+    flw.cp[wat] = m_mix - lo;
+    flw.maxflow();
 
-    for (int i = 2; i <= 2*m; i += 2) {
-        res[i][0] = flw.fl[i] / v;
-        if (flw.fl[i] < -eps) {
-            flw.cp[i+1] -= flw.fl[i+1];
-            flw.cp[i] = 0;
-        } else if (flw.fl[i] > eps) {
-            flw.cp[i] -= flw.fl[i];
-            flw.cp[i+1] = 0;
+    for (int u = 1; u < flw.n; u++) {
+        for (int ed = flw.hd[u]; ed; ed = flw.nx[ed]) {
+            if (flw.to[ed] && flw.fl[ed] < -eps)
+                deg[u]++;
         }
     }
 
-    memset(flw.fl, 0, sizeof flw.fl);
-    flw.cp[flu] = 0;
-    flw.cp[wat] = m_mix - lo;
-    assert(abs(flw.maxflow() - (m_mix - lo)) < eps);
+    queue<int> qu;
+    inp[1][0] = lo; 
+    inp[2][1] = m_mix - lo;
 
-    cout << setprecision(20) << fixed;
+    for (int u = 1; u < flw.n; u++) {
+        if (deg[u] == 0)
+            qu.push(u);
+    }
+
+    while (!qu.empty()) {
+        int u = qu.front();
+        qu.pop();
+
+        debug cout << "vertex " << u << " receives " << inp[u][0] << " " << inp[u][1] << endl;
+
+        for (int k = 0; k < 2; k++) {
+            for (int ed = flw.hd[u]; ed; ed = flw.nx[ed]) {
+                double qt = min(inp[u][k], flw.fl[ed]);
+                if (qt < eps) continue;
+                res[ed][k] += qt;
+                inp[u][k] -= qt;
+                flw.fl[ed] -= qt;
+                inp[flw.to[ed]][k] += qt;
+            }
+        }
+
+        for (int ed = flw.hd[u]; ed; ed = flw.nx[ed]) {
+            deg[flw.to[ed]]--;
+            if (deg[flw.to[ed]] == 0)
+                qu.push(flw.to[ed]);
+        }
+    }
+
+    debug cout << "=================" << endl;
     for (int i = 2; i <= 2*m; i += 2) {
-        if (res[i][0] < -eps || flw.fl[i] < -eps)
-            assert(res[i][0] < eps && flw.fl[i] < eps);
-        if (res[i][0] > eps || flw.fl[i] > eps)
-            assert(res[i][0] > -eps && flw.fl[i] > -eps);
-        cout << res[i][0] << " " << flw.fl[i] << endl;
+        debug {
+            if (res[i][0] < -eps || flw.fl[i] < -eps)
+                assert(res[i][0] < eps && flw.fl[i] < eps);
+            if (res[i][0] > eps || flw.fl[i] > eps)
+                assert(res[i][0] > -eps && flw.fl[i] > -eps);
+        }
+        debug cout << flw.to[i+1] << " " << flw.to[i] << " ";
+        cout << (res[i][0] - res[i+1][0])/v << " " << (res[i][1] - res[i+1][1]) << endl;
     }
     cout << calc(lo) << endl;
 }
