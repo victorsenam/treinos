fb8d48e734026d23ded7f3e340a0a6627fe7dc20
Victor Sena Molero <victorsenam@gmail.com>
2018-02-16 21:30:57 -0200

    F com intersecção em O(1)

--- ojs/codeforces/932/F/old.cpp
+++ ojs/codeforces/932/F/old.cpp
@@ -16,8 +16,8 @@ struct line {
 struct envelope { // min envelope, the first element is the best at the beggining
 	deque<line> qu; num lo, hi; envelope (num a, num b) : lo(a), hi(b) {}
 	num inter (line a, line b) { assert(!(b<a)); // first point where b beats (properly) a XXX double
-		if (b.a == a.a) return (a.b < b.b)?hi+1:lo;
-		return max(min((b.b - a.b - (b.b - a.b<0)*(a.a - b.a - 1))/(a.a - b.a) + 1,hi+1),lo);
+		//if (b.a == a.a) return (a.b < b.b)?hi+1:lo;
+		//return max(min((b.b - a.b + (b.b-a.b>0)*(a.a-b.a-1))/(a.a - b.a),hi+1),lo);
 		num lo = this->lo; num hi = this->hi + 1;
 		while (lo < hi) {
 			num md = lo + (hi - lo)/2;
@@ -29,27 +29,22 @@ struct envelope { // min envelope, the first element is the best at the begginin
 	void push_back (line l) { assert(qu.empty() || !(l < qu.back())); // insert a line that is best at hi or never
 		if (!qu.empty() && qu.back()(hi) <= l(hi)) return;
 		for (num x; qu.size() && l(x = (qu.size()==1?lo:inter(qu[qu.size()-2],qu.back()))) < qu.back()(x); qu.pop_back()); // XXX double
+		/*
+		num x;
+		while (qu.size()) {
+			num x = (qu.size()==1?lo:inter(qu[qu.size()-2],qu.back()));
+			if (qu.back()(x) <= l(x)) break;
+			qu.pop_back();
+		}
+		*/
 		qu.push_back(l);
-	}
-	void push_front (line l) { assert(qu.empty() || !(qu.front() < l)); // insert a line that is best at lo or never
-		if (!qu.empty() && qu.front()(lo) < l(lo)) return;
-		for (num x; qu.size() && l(x = (qu.size()==1?hi:inter(qu[0],qu[1]))) <= qu.front()(x); qu.pop_front()); // XXX double
-		qu.push_front(l);
-	}
-	line pop_front (num lo) { assert(!qu.empty() && this->lo <= lo + eps); // gets best line at this new lo
-		for (this->lo = lo; qu.size() > 1 && qu[0](lo) > qu[1](lo); qu.pop_front());
-		return qu.front();
-	}
-	line pop_back (num hi) { assert(!qu.empty() && this->hi >= hi - eps); // gets best line at this new hi
-		for (this->hi = hi; qu.size() > 1 && qu[qu.size()-2](hi) <= qu.back()(hi); qu.pop_back());
-		return qu.back();
 	}
 	line get (num x) { assert(!qu.empty() && lo <= x + eps && x <= hi + eps); // gets best line at x
 		return (*lower_bound(qu.begin(),qu.end()-1,qu[0],[x](line & a, const line & o){ return a(x) > (*((&a)+1))(x); }));
 	}
 };
 struct full_envelope {
-	vector<envelope> v; full_envelope() {}
+	vector<envelope> v; full_envelope(envelope c) {v.pb(c);}; full_envelope() {};
 	int siz;
 	void add (line l) {
 		siz++;
@@ -57,19 +52,20 @@ struct full_envelope {
 		while (v.size() && v.back().qu.size() <= nw.qu.size()) {
 			envelope aux(nw.lo,nw.hi); auto jt = nw.qu.begin();
 			for (line r : v.back().qu) {
-				while (jt != nw.qu.end() && *jt < r) aux.push_back(*(jt++));
+				for (; jt != nw.qu.end() && *jt < r; ++jt) aux.pb(*jt);
 				aux.push_back(r);
 			}
-			while (jt != nw.qu.end()) aux.push_back(*(jt++));
+			for (; jt != nw.qu.end(); ++jt) aux.pb(*jt);
 			nw = aux; v.pop_back();
 		}
 		v.push_back(nw);
 	}
 	line get (num x) { // you can easily use pop_front and pop_back here
+		assert(v.size() > 0);
 		line b = v.front().get(x);
 		for (int i = 1; i < v.size(); i++) {
 			line c = v[i].get(x);
-			if (c(x)!=b(x)?c(x)<b(x):c<b) b = c;
+			if (c(x)<b(x)) b = c;
 		}
 		return b;
 	}
@@ -77,41 +73,48 @@ struct full_envelope {
 
 const int N = 1e5+7;
 full_envelope env[N];
-int wh[N];
-int n;
 ll a[N], b[N];
+int wh[N];
 vector<int> adj[N];
+int n;
 ll ans[N];
 
-void dfs (int u, int p) {
-	env[u].v.pb(envelope(-1e5,1e5));
+int dfs (int u, int p) {
 	wh[u] = u;
+	env[u] = full_envelope(envelope(-2e5,2e5));
+	env[u].siz = 0;
 	for (int v : adj[u]) if (v != p) {
 		dfs(v,u);
 		if (env[wh[u]].siz < env[wh[v]].siz) swap(wh[u],wh[v]);
-		for (envelope e : env[wh[v]].v) for (line l : e.qu)
+		for (envelope & e : env[wh[v]].v) for (line l : e.qu) {
 			env[wh[u]].add(l);
+		}
 	}
-	if (u && adj[u].size() == 1) ans[u] = 0;
-	else ans[u] = env[wh[u]].get(a[u])(a[u]);
+	if (u && adj[u].size() == 1) {
+		ans[u] = 0;
+	} else {
+		ans[u] = env[wh[u]].get(a[u])(a[u]);
+	}
 	env[wh[u]].add({b[u],ans[u]});
 }
 
 int main () {
 	scanf("%d", &n);
-
 	for (int i = 0; i < n; i++)
 		scanf("%lld", &a[i]);
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) {
 		scanf("%lld", &b[i]);
-
+	}
 	for (int i = 0; i < n-1; i++) {
-		int x, y; scanf("%d %d", &x, &y); x--; y--;
-		adj[x].pb(y); adj[y].pb(x);
+		int x, y;
+		scanf("%d %d", &x, &y);
+		x--; y--;
+		adj[x].pb(y);
+		adj[y].pb(x);
 	}
 
 	dfs(0,0);
-
-	for (int i = 0; i < n; i++) printf("%lld ", ans[i]);
+	for (int i = 0; i < n; i++)
+		printf("%lld ", ans[i]);
 	printf("\n");
 }
