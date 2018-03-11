46c357755ecc8cbd1bd475c7f512a3b68f3e1e6a
Victor Sena Molero <victorsenam@gmail.com>
2018-03-11 08:51:50 -0300

    Erro

--- ojs/poj/3418/3418.cpp
+++ ojs/poj/3418/3418.cpp
@@ -1,10 +1,6 @@
-//#include <bits/stdc++.h>
-#include <cstdio>
-#include <vector>
 #include <queue>
+#include <cstdio>
 #include <cassert>
-#include <algorithm>
-#include <cmath>
 #include <cctype>
 #define cout if (1) cout
 
@@ -13,68 +9,62 @@ typedef long long int ll;
 typedef pair<ll,ll> pii;
 #define pb push_back
 
-typedef ll num; const num eps = 0;
-// XXX double warnings means there's some specific integer operation (+1, lo<hi, etc...), not precision-related
-struct line {
-	num a, c;
-	bool hi;
-	num operator () (num x) const { return a*x*x + (a+a)*x + c; }
-	bool operator < (const line & ot) const { return hi?a>ot.a:a<ot.a; } // decreasing slope, if function ties, the smallest in this order is chosen
-};
-num glx;
-bool compare (line & a, const line & o) { return a(glx) > (*((&a)+1))(glx); }
-struct envelope { // min envelope, the first element is the best at the beggining
-	deque<line> qu; num lo, hi; envelope (num a, num b) : lo(a), hi(b) {}
-	inline num inter (line a, line b) { assert(!(b<a)); // first point where b beats (properly) a XXX double
-		if (a.a == b.a) return (a.c<b.c)?hi+1:lo;
-		long double c = double(b.c-a.c)/double(a.a-b.a);
-		long double delta = sqrt(4.*(1-c));
-		if (delta != delta) return a.a<b.a?hi+1:lo;
-		num p = ll(-1+(a.hi?ceil(delta/2):-floor(delta/2)));
-		return max(min(p,hi+1),lo);
-	}
-	void push_back (line l) { assert(qu.empty() || !(l < qu.back())); // insert a line that is best at hi or never
-		if (!qu.empty() && qu.back()(hi) <= l(hi)) return;
-		for (num x; qu.size() && l(x = (qu.size()==1?lo:inter(qu[qu.size()-2],qu.back()))) < qu.back()(x); qu.pop_back()); // XXX double
-		qu.push_back(l);
-	}
-	void push_front (line l) { assert(qu.empty() || !(qu.front() < l)); // insert a line that is best at lo or never
-		if (!qu.empty() && qu.front()(lo) < l(lo)) return;
-		for (num x; qu.size() && l(x = (qu.size()==1?hi:inter(qu[0],qu[1]))) <= qu.front()(x); qu.pop_front()); // XXX double
-		qu.push_front(l);
-	}
-	line pop_front (num lo) { assert(!qu.empty() && this->lo <= lo + eps); // gets best line at this new lo
-		for (this->lo = lo; qu.size() > 1 && qu[0](lo) > qu[1](lo); qu.pop_front());
-		return qu.front();
-	}
-	line pop_back (num hi) { assert(!qu.empty() && this->hi >= hi - eps); // gets best line at this new hi
-		for (this->hi = hi; qu.size() > 1 && qu[qu.size()-2](hi) <= qu.back()(hi); qu.pop_back());
-		return qu.back();
-	}
-	line get (num x) { assert(!qu.empty() && lo <= x + eps && x <= hi + eps); // gets best line at x
-		glx = x;
-		return (*lower_bound(qu.begin(),qu.end()-1,qu[0],compare));
-	}
-};
-struct full_envelope {
-	vector<envelope> v; full_envelope(envelope c) { v.pb(c); }
-	void add (line l) {
-		envelope nw(v[0].lo,v[0].hi); nw.push_back(l);
-		while (v.size() && v.back().qu.size() <= nw.qu.size()) {
-			envelope aux(nw.lo,nw.hi); deque<line>::iterator jt = nw.qu.begin();
-			for (int i = 0; i < v.back().qu.size(); i++) {
-				line r = v.back().qu[i];
-				while (jt != nw.qu.end() && *jt < r) aux.push_back(*(jt++));
-				aux.push_back(r);
-			}
-			while (jt != nw.qu.end()) aux.push_back(*(jt++));
-			swap(nw.qu,aux.qu); v.pop_back();
+typedef ll num; const num eps = 0; num INF = 1e17;
+// XXX double: indicates operations specific to integers (+1,lo<hi,...), non precision-related
+template<typename line> struct envelope { // if line is used, the template tag can be simply removed
+	deque<line> q; num lo,hi; envelope (num _lo, num _hi) : lo(_lo), hi(_hi) {}
+	void push_back (line l) { assert(q.empty() || !(l<q.back())); // l is best at hi or never
+		if (!q.empty() && q.back()(hi) <= l(hi)) return;
+		for (num x; q.size(); q.pop_back()) {
+			x = (q.size()<=1?lo:min(max(lo,next(q.rbegin())->inter(q.back())),hi+1));
+			if (l(x) >= q.back()(x)) break;
 		}
-		v.push_back(nw);
+		q.push_back(l);
 	}
-	line get (num x) { // you can easily use pop_front and pop_back here
+	line get (num x) {
+		int lo = 0, hi = q.size()-1;
+		while (lo < hi) { int md = (lo+hi)/2;
+			if (q[md](x) > q[md+1](x)) lo = md+1;
+			else hi = md;
+		}
+		return q[lo];
+	}
+};
+struct line { // change only parameters and () for any viable functions, env operations in amort. O(lg(hi-lo))
+	num a,c; envelope<line> * env; num operator () (num x) const { return a*x*x + (a+a)*x + c; }
+	line (num _a, num _c, envelope<line> * _env) : a(_a), c(_c), env(_env) {}
+	inline bool operator < (const line & ot) const { return (*this)(env->lo) < ot(env->lo) && (*this)(env->hi) > ot(env->hi); } // first element is best at lo
+	num inter (line o) { assert(!(o<(*this)));
+		num lo = env->lo, hi = env->hi + 1;
+		while (lo < hi) {
+			num md = lo+(hi-lo)/2;
+			if ((*this)(md)<=o(md)) lo = md+1;
+			else hi = md;
+		}
+		return lo;
+	}
+};
+template<typename T> inline T prev (T it) { return --it; }
+template<typename T> inline T next (T it) { return ++it; }
+template<typename line> struct full_envelope {
+	deque<envelope<line> > v; full_envelope(envelope<line> c) {v.pb(c);}
+	void add (line l) { // amort. O(lg(n)*inter)
+		v.push_back(envelope<line>(v[0].lo,v[0].hi)); v.back().push_back(l);
+		while (v.size() > 1 && prev(prev(v.end()))->q.size() <= prev(v.end())->q.size()) {
+			deque<line> aux; swap(aux,prev(prev(v.end()))->q);
+			typename deque<line>::iterator it = aux.begin();
+			typename deque<envelope<line> >::iterator r = prev(prev(v.end()));
+			for (unsigned int i = 0; i < v.back().q.size(); i++) { line l = v.back().q[i];
+				while (it != aux.end() && *it < l) r->push_back(*(it++));
+				r->push_back(l);
+			}
+			while (it != aux.end()) r->push_back(*(it++));
+			v.pop_back();
+		}
+	}
+	line get (num x) { // O(lg(n)^2) use pop_back/pop_front for better time
 		line b = v.front().get(x);
-		for (int i = 1; i < v.size(); i++) {
+		for (int i = 1; i < (int) v.size(); i++) {
 			line c = v[i].get(x);
 			if (c(x)<b(x)) b = c;
 		}
@@ -88,21 +78,21 @@ void rd (ll & x) {
 	do { x = (x << 8) + (x << 2) + c - '0'; }
 	while (isdigit(c = getchar()));
 }
-
 void rd (char & c) { while(isspace(c = getchar())); }
 
 int main () {
 	ll n;
 	rd(n);
 
-	full_envelope low(envelope(-1e6,-1)), high(envelope(0,1e6));
+	full_envelope<line> low(envelope<line>(-1e6,-1)), high(envelope<line>(0,1e6));
 	for (int i = 0; i < n; i++) {
 		char q;
 		rd(q);
 		if (q == 'I') {
 			ll a, b, c;
 			rd(a); rd(b); rd(c);
-			low.add({a,c,0}); high.add({a,c,1});
+			low.add(line(a,c,&low.v[0]));
+			high.add(line(a,c,&high.v[0]));
 		} else {
 			ll x;
 			rd(x);
